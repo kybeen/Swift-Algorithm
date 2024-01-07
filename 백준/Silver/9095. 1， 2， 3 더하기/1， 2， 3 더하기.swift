582bd061b51138f199420258683168e273085d25
kybeen <89764127+kybeen@users.noreply.github.com>
2024-01-07 16:13:15 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 12 ms, Memory: 79508 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.swift
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.swift
@@ -1,24 +1,16 @@
 import Foundation
 
 let t = Int(readLine()!)!
+
+var dp = [Int](repeating: 0, count: 11)
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
+for i in 4...10 {
+    dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+}
+
 for _ in 0..<t {
     let n = Int(readLine()!)!
-    switch n {
-    case 1:
-        print(1)
-    case 2:
-        print(2)
-    case 3:
-        print(4)
-    default:
-        // dp[n] : n을 1,2,3의 합으로 나타내는 방법의 수
-        var dp = [Int](repeating: 0, count: n+1)
-        dp[1] = 1
-        dp[2] = 2
-        dp[3] = 4
-        for i in 4...n {
-            dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
-        }
-        print(dp[n])
-    }
+    print(dp[n])
 }
