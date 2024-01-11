106e53399d8d097782fa2d047c03e4b1f115bf7e
kybeen <89764127+kybeen@users.noreply.github.com>
2024-01-11 16:40:06 +0900

    [Silver III] Title: 모든 순열, Time: 108 ms, Memory: 79508 KB -BaekjoonHub

---
+++ 백준/Silver/10974. 모든 순열/모든 순열.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+let n = Int(readLine()!)!
+
+var arr = [Int]()
+func dfs(_ depth: Int) {
+    if arr.count == n {
+        print(arr.map{String($0)}.joined(separator: " "))
+        return
+    }
+    
+    for i in 1...n {
+        if !arr.contains(i) {
+            arr.append(i)
+            dfs(depth+1)
+            arr.popLast()
+        }
+    }
+}
+
+dfs(0)
