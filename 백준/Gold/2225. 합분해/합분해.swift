55aa42640d032f392488421918f6ce940be3f2b8
kybeen <89764127+kybeen@users.noreply.github.com>
2024-01-22 01:37:44 +0900

    [Gold V] Title: 합분해, Time: 12 ms, Memory: 79776 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.swift
+++ 백준/Gold/2225. 합분해/합분해.swift
@@ -1,23 +1,21 @@
 import Foundation
 
 let nums = readLine()!.split(separator: " ").map{ Int($0)! }
-let n = nums[0]
-let k = nums[1]
+let N = nums[0]
+let K = nums[1]
 
-// dp[i][j] : 정수 j개를 더해서 그 합이 i가 되는 경우의 수
-var dp = [[Int]](repeating: [Int](repeating: 0, count: k+1), count: n+1)
-dp[0] = [Int](repeating: 1, count: k+1)
-dp[0][0] = 0
-for i in 1...n {
-    dp[i][1] = 1
+// dp[i][j] : 정수 i개를 더해서 합이 j가 되는 경우의 수
+var dp = [[Int]](repeating: [Int](repeating: 0, count: N+1), count: K+1)
+dp[1] = [Int](repeating: 1, count: N+1)
+for i in 0...K {
+    dp[i][0] = 1
 }
 
-if k > 1 {
-    for i in 1...n {
-        for j in 2...k {
+if K > 1 {
+    for i in 2...K {
+        for j in 1...N {
             dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % 1000000000
         }
     }
 }
-
-print(dp[n][k])
+print(dp[K][N] % 1000000000)
