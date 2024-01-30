2d05dd6d409e4630bafeda3e9994d5c2af3838c6
kybeen <89764127+kybeen@users.noreply.github.com>
2024-01-30 17:30:26 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 60 ms, Memory: 84776 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.swift
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.swift
@@ -6,50 +6,36 @@ let N = NK[0]
 let K = NK[1]
 
 var idx = 0
-var queue = [(Int, Int)]() // (현재 위치, 걸린 시간)
+var queue = [Int]()
 var visited = [Int](repeating: -1, count: 100001) // -1: 방문 안함, -2: 출발지, 나머지 숫자: 이전 위치
 
-queue.append((N, 0))
+queue.append(N)
 visited[N] = -2
 var result = [String]()
 
 while idx < queue.count {
     let now = queue[idx]
     
-    // 경로를 역추적해서 결과 배열에 저장해준다.
-    func printResult(_ now: Int) {
-        // 출발지로 돌아오면 종료
-        if visited[now] == -2 {
-            result.append(String(now))
-            return
+    // K에 도착하면 경로를 역추적해서 결과 배열에 저장해준다.
+    if now == K {
+        var i = now
+        while i != -2 {
+            result.append(String(i))
+            i = visited[i]
         }
-        result.append(String(now))
-        printResult(visited[now])
-    }
-    if now.0 == K {
-        print(now.1) // 걸린 시간 출력
-        printResult(now.0)
         break
     }
     
-    let nextA = now.0 - 1
-    let nextB = now.0 + 1
-    let nextC = now.0 * 2
-    
-    func goBFS(_ next: Int) {
-        if 0...100000 ~= next {
-            if visited[next] == -1 {
-                queue.append((next, now.1+1))
-                visited[next] = now.0 // visited 배열에 이전 경로를 표시
-            }
+    var nextCase = [-1, 1, now]
+    for d in nextCase {
+        if 0...100000 ~= now+d && visited[now+d] == -1 {
+            queue.append(now+d)
+            visited[now+d] = now // visited 배열에 이전 경로를 표시
         }
     }
-    
-    goBFS(nextA)
-    goBFS(nextB)
-    goBFS(nextC)
     
     idx += 1
 }
 
+print(result.count - 1)
 print(result.reversed().joined(separator: " "))
