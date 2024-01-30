0e45d1c11188741a209ac7c052ca1a2c3784d0b5
kybeen <89764127+kybeen@users.noreply.github.com>
2024-01-30 20:20:45 +0900

    [Gold IV] Title: 이모티콘, Time: 16 ms, Memory: 84164 KB -BaekjoonHub

---
+++ 백준/Gold/14226. 이모티콘/이모티콘.swift
@@ -0,0 +1,41 @@
+import Foundation
+
+let S = Int(readLine()!)!
+
+var idx = 0
+var queue = [(Int, Int, Int)]() // (현재 이코티콘 개수, 클립보드의 이모티콘 수, 걸린 시간)
+var visited = [[Bool]](repeating: [Bool](repeating: false, count: 2001), count: 2001)
+
+queue.append((1, 0, 0))
+visited[1][0] = true
+
+while idx < queue.count {
+    let now = queue[idx]
+    
+    if now.0 == S {
+        print(now.2)
+        exit(0)
+    }
+    
+    if 1...2000 ~= now.0 {
+        // 1. 화면에 있는 이모티콘을 모두 복사해서 클립보드에 저장한다.
+        if !visited[now.0][now.0] {
+            queue.append((now.0, now.0, now.2+1))
+            visited[now.0][now.0] = true
+        }
+        // 3. 화면에 있는 이모티콘 중 하나를 삭제한다.
+        if !visited[now.0-1][now.1] {
+            queue.append((now.0-1, now.1, now.2+1))
+            visited[now.0-1][now.1] = true
+        }
+    }
+    if now.1 > 0 && now.0+now.1 <= 2000 {
+        // 2. 클립보드에 있는 모든 이모티콘을 화면에 붙여넣기 한다.
+        if !visited[now.0+now.1][now.1] {
+            queue.append((now.0+now.1, now.1, now.2+1))
+            visited[now.0+now.1][now.1] = true
+        }
+    }
+    
+    idx += 1
+}
