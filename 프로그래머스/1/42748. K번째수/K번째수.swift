e418d2d38b151ab1ba94c5d7074d321c81c6b9d1
kybeen <89764127+kybeen@users.noreply.github.com>
2024-02-01 02:29:33 +0900

    [level 1] Title: K번째수, Time: 0.13 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+func solution(_ array:[Int], _ commands:[[Int]]) -> [Int] {
+    var result = [Int]()
+    
+    for command in commands {
+        let i = command[0] - 1
+        let j = command[1] - 1
+        let k = command[2] - 1
+        
+        let sliceArray = array[i...j].sorted()
+        result.append(sliceArray[k])
+    }
+    
+    
+    return result
+}
