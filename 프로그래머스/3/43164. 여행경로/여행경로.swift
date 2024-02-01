6529c2dc8aa4d533181321455ce6f7b03ddfd6e9
kybeen <89764127+kybeen@users.noreply.github.com>
2024-02-01 15:38:52 +0900

    [level 3] Title: 여행경로, Time: 0.13 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.swift
+++ 프로그래머스/3/43164. 여행경로/여행경로.swift
@@ -1,23 +1,21 @@
 import Foundation
 
 func solution(_ tickets:[[String]]) -> [String] {
-    let n = tickets.count
+    let tickets = tickets.sorted { $0[1] < $1[1] } // 도착지를 기준으로 정렬
+    let n = tickets.count // 티켓 개수
     
+    var flag = false
     var result = [String]()
-    var resultPath = String(repeating: "ZZZ", count: n+1) // 경로를 모두 이어 붙인 하나의 String (알파벳 순서 확인용)
-    var stack = [String]()
+    var stack = ["ICN"] // 출발지는 "ICN" 고정이기 때문에 미리 넣어둠
     var visited = [Bool](repeating: false, count: n)
+    
     func dfs(_ now: [String], _ depth: Int) {
+        guard !flag else { return }
+        
         // 티켓을 모두 사용했을 때
         if depth == n {
-            stack.append(now[1]) // 마지막 도착지는 따로 추가해줘야함
-            let nowPath = stack.joined(separator: "")
-            // 알파벳 순으로 앞서는 경로 갱신
-            if nowPath < resultPath {
-                resultPath = nowPath
-                result = stack
-            }
-            stack.popLast()
+            result = stack
+            flag = true
             return
         }
         
@@ -25,7 +23,7 @@ func solution(_ tickets:[[String]]) -> [String] {
             if !visited[i] {
                 // 현재 티켓과 이어서 쓸 수 있는 경로라면 진행
                 if tickets[i][0] == now[1] {
-                    stack.append(tickets[i][0])
+                    stack.append(tickets[i][1])
                     visited[i] = true
                     dfs(tickets[i], depth+1)
                     visited[i] = false
@@ -38,7 +36,7 @@ func solution(_ tickets:[[String]]) -> [String] {
     // "ICN"에서 출발하는 티켓을 기준으로 DFS 시작
     for i in 0..<n {
         if tickets[i][0] == "ICN" {
-            stack.append(tickets[i][0])
+            stack.append(tickets[i][1])
             visited[i] = true
             dfs(tickets[i], 1)
             visited[i] = false
@@ -48,3 +46,60 @@ func solution(_ tickets:[[String]]) -> [String] {
     
     return result
 }
+
+
+
+
+
+
+
+// import Foundation
+
+// func solution(_ tickets:[[String]]) -> [String] {
+//     let n = tickets.count
+    
+//     var result = [String]()
+//     var resultPath = String(repeating: "ZZZ", count: n+1) // 경로를 모두 이어 붙인 하나의 String (알파벳 순서 확인용)
+//     var stack = [String]()
+//     var visited = [Bool](repeating: false, count: n)
+//     func dfs(_ now: [String], _ depth: Int) {
+//         // 티켓을 모두 사용했을 때
+//         if depth == n {
+//             stack.append(now[1]) // 마지막 도착지는 따로 추가해줘야함
+//             let nowPath = stack.joined(separator: "")
+//             // 알파벳 순으로 앞서는 경로 갱신
+//             if nowPath < resultPath {
+//                 resultPath = nowPath
+//                 result = stack
+//             }
+//             stack.popLast()
+//             return
+//         }
+        
+//         for i in 0..<n {
+//             if !visited[i] {
+//                 // 현재 티켓과 이어서 쓸 수 있는 경로라면 진행
+//                 if tickets[i][0] == now[1] {
+//                     stack.append(tickets[i][0])
+//                     visited[i] = true
+//                     dfs(tickets[i], depth+1)
+//                     visited[i] = false
+//                     stack.popLast()
+//                 }
+//             }
+//         }
+//     }
+    
+//     // "ICN"에서 출발하는 티켓을 기준으로 DFS 시작
+//     for i in 0..<n {
+//         if tickets[i][0] == "ICN" {
+//             stack.append(tickets[i][0])
+//             visited[i] = true
+//             dfs(tickets[i], 1)
+//             visited[i] = false
+//             stack.popLast()
+//         }
+//     }
+    
+//     return result
+// }
