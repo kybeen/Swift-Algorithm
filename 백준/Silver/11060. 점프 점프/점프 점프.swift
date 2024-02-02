b60e46347e90378e67c77e6a8c42495c42f6e11d
kybeen <89764127+kybeen@users.noreply.github.com>
2024-02-02 21:12:53 +0900

    [Silver II] Title: 점프 점프, Time: 12 ms, Memory: 79516 KB -BaekjoonHub

--- 백준/Silver/11060. 점프 점프/점프 점프.swift
+++ 백준/Silver/11060. 점프 점프/점프 점프.swift
@@ -3,8 +3,9 @@ import Foundation
 let N = Int(readLine()!)!
 let A = readLine()!.split(separator: " ").map { Int($0)! }
 
-// dp[i] : i번 칸으로 가기 위한 최소 점프 횟수
-var dp = [Int](repeating: 0, count: N)
+// dp[i] : i번 칸으로 가기 위한 최소 점프 횟수 (N의 최대값이 1000이라서 최대 점프 횟수는 999라고 보면 됨)
+var dp = [Int](repeating: 1001, count: N)
+dp[0] = 0
 if N > 1 {
     for i in 1..<N {
         for j in 0..<i {
@@ -14,17 +15,13 @@ if N > 1 {
             }
             // j번 칸에서 점프해서 갈 수 있는 거리라면 dp테이블에 최솟값으로 점프 횟수 갱신
             if j + A[j] >= i {
-                if dp[i] == 0 {
-                    dp[i] = dp[j] + 1
-                } else {
-                    dp[i] = min(dp[i], dp[j]+1)
-                }
+                dp[i] = min(dp[i], dp[j]+1)
             }
         }
     }
 }
 
-if N > 1 && dp[N-1] == 0 {
+if dp[N-1] == 1001 {
     print(-1)
 } else {
     print(dp[N-1])
