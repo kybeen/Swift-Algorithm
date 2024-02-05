aa7859fac5219fd73a8240a1c19b077f4ad874f8
kybeen <89764127+kybeen@users.noreply.github.com>
2024-02-05 17:41:46 +0900

    [Gold IV] Title: 팰린드롬?, Time: 480 ms, Memory: 124704 KB -BaekjoonHub

---
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.swift
@@ -0,0 +1,107 @@
+import Foundation
+
+// 라이노님의 FileIO
+final class FileIO {
+    private var buffer:[UInt8]
+    private var index: Int
+    
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        buffer = Array(fileHandle.readDataToEndOfFile())+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+        index = 0
+    }
+    
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+        
+        return buffer.withUnsafeBufferPointer { $0[index] }
+    }
+    
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+        
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45{ isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+        
+        return sum * (isPositive ? 1:-1)
+    }
+    
+    @inline(__always) func readString() -> String {
+        var str = ""
+        var now = read()
+        
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        
+        while now != 10
+                && now != 32 && now != 0 {
+            str += String(bytes: [now], encoding: .ascii)!
+            now = read()
+        }
+        
+        return str
+    }
+}
+let file = FileIO()
+
+let N = file.readInt()
+var nums: [Int] = [-1]
+for _ in 0..<N {
+    nums.append(file.readInt())
+}
+let M = file.readInt()
+
+// dp[i][j] : S=i, E=j 일 때의 팰린드롬 여부
+var dp = [[Int]](repeating: [Int](repeating: 0, count: N+1), count: N+1)
+
+for i in 1...N {
+    // S==E 일 때는 무조건 팰린드롬
+    dp[i][i] = 1
+    // S~E가 짝수개일 경우에는 중앙의 2개 수가 같은지 확인해서 체크해줘야함
+    if i < N && nums[i] == nums[i+1]{
+        dp[i][i+1] = 1
+    }
+}
+
+for i in stride(from: N-1, through: 2, by: -1) {
+    var holOffset = 1
+    var zzakOffset = 2
+    
+    // S~E가 홀수개인 경우 확인
+    while i+holOffset <= N {
+        if nums[i-holOffset] == nums[i+holOffset] {
+            dp[i-holOffset][i+holOffset] = 1
+        } else {
+            break
+        }
+        
+        holOffset += 1
+    }
+    
+    // S~E가 짝수개인 경우 확인
+    if dp[i][i+1] == 1 && i <= N-2 {
+        while i+zzakOffset <= N {
+            if nums[i-zzakOffset+1] == nums[i+zzakOffset] {
+                dp[i-zzakOffset+1][i+zzakOffset] = 1
+            } else {
+                break
+            }
+            
+            zzakOffset += 1
+        }
+    }
+}
+
+var result = ""
+for _ in 0..<M {
+    let (S, E) = (file.readInt(), file.readInt())
+    result += "\(dp[S][E])\n"
+}
+
+print(result)
