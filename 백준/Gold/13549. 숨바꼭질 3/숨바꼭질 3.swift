80b530ba2b837d0f7bdc4dc153eb454ff6264346
kybeen <89764127+kybeen@users.noreply.github.com>
2024-02-06 09:51:16 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 20 ms, Memory: 82788 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.swift
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.swift
@@ -4,14 +4,15 @@ let NK = readLine()!.split(separator: " ").map { Int($0)! }
 let N = NK[0]
 let K = NK[1]
 
+var idx = 0
 var queue = [(Int, Int)]() // (현재 위치, 걸린 시간)
 var visited = [Bool](repeating: false, count: 100_001)
 
 queue.append((N, 0))
 visited[N] = true
 
-while !queue.isEmpty {
-    let now = queue.removeFirst()
+while idx < queue.count {
+    let now = queue[idx]
     
     if now.0 == K {
         print(now.1)
@@ -21,7 +22,7 @@ while !queue.isEmpty {
     // 2*X 위치로 이동하는 경우 걸리는 시간이 0초이기 때문에 이 경우를 먼저 확인
     if 0...100_000 ~= now.0*2 && !visited[now.0*2] {
         visited[now.0*2] = true
-        queue.insert((now.0*2, now.1), at: 0)
+        queue.append((now.0*2, now.1))
     }
     if 0...100_000 ~= now.0-1 && !visited[now.0-1] {
         visited[now.0-1] = true
@@ -31,4 +32,6 @@ while !queue.isEmpty {
         visited[now.0+1] = true
         queue.append((now.0+1, now.1+1))
     }
+    
+    idx += 1
 }
