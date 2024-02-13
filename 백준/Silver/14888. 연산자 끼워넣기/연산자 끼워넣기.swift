e72fcf220e58e44297663af48fdfe53e1547a0ba
kybeen <89764127+kybeen@users.noreply.github.com>
2024-02-14 03:20:56 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 16 ms, Memory: 79516 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.swift
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.swift
@@ -2,37 +2,38 @@ import Foundation
 
 let N = Int(readLine()!)!
 let A = readLine()!.split(separator: " ").map { Int($0)! }
-var operators = readLine()!.split(separator: " ").map { Int($0)! } // [+, -, *, /] 순서
+var operators = readLine()!.split(separator: " ").map { Int($0)! } // +, -, *, / 순서
 
 var minResult = Int.max
 var maxResult = Int.min
-var stack = [Int]() // 0:+, 1:-, 2:*, 3:/
 
-func dfs(_ depth: Int, _ nowValue: Int) {
-    
+func dfs(_ depth: Int, _ result: Int) {
+    // 모든 연산이 끝나면 최대/최소 갱신
     if depth == N-1 {
-        minResult = min(minResult, nowValue)
-        maxResult = max(maxResult, nowValue)
+        minResult = min(minResult, result)
+        maxResult = max(maxResult, result)
     }
     
     for i in 0..<4 {
+        // 사용할 수 있는 연산자가 남아 있으면
         if operators[i] > 0 {
-            stack.append(i)
             operators[i] -= 1
+            
+            // 해당 연산자를 적용한 결과를 전달하며 dfs 진행
             switch i {
-            case 0: // +
-                dfs(depth+1, nowValue + A[depth+1])
-            case 1: // -
-                dfs(depth+1, nowValue - A[depth+1])
-            case 2: // *
-                dfs(depth+1, nowValue * A[depth+1])
-            case 3: // /
-                dfs(depth+1, nowValue / A[depth+1])
+            case 0:
+                dfs(depth+1, result+A[depth+1])
+            case 1:
+                dfs(depth+1, result-A[depth+1])
+            case 2:
+                dfs(depth+1, result*A[depth+1])
+            case 3:
+                dfs(depth+1, result/A[depth+1])
             default:
                 break
             }
+            
             operators[i] += 1
-            _ = stack.popLast()
         }
     }
 }
