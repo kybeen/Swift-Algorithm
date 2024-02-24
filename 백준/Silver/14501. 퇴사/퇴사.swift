c567760bc3ffeef971005ab16953942acb069676
kybeen <89764127+kybeen@users.noreply.github.com>
2024-02-24 17:18:53 +0900

    [Silver III] Title: 퇴사, Time: 12 ms, Memory: 79516 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.swift
+++ 백준/Silver/14501. 퇴사/퇴사.swift
@@ -1,36 +1,22 @@
 import Foundation
 
 let N = Int(readLine()!)!
-var T = [Int]()
-var P = [Int]()
+var T: [Int] = [0]
+var P: [Int] = [0]
 for _ in 0..<N {
     let TP = readLine()!.split(separator: " ").map { Int($0)! }
     T.append(TP[0])
     P.append(TP[1])
 }
 
-// dp[i] : i일에 상담을 했을 때 받을 수 있는 최대 금액
-var dp = [Int](repeating: 0, count: N)
-
-// 상담 기간이 퇴사일을 넘어가는 경우 0으로 처리
-for i in 0..<N {
-    if i+T[i]-1 >= N {
-        dp[i] = 0
-    } else {
-        dp[i] = P[i]
-    }
-}
-
-for i in 1..<N {
-    if dp[i] == 0 {
-        continue
-    } else {
-        for j in 0..<i {
-            // 상담을 진행해도 i날에 지장이 없는 일수라면 최대값 비교
-            if j+T[j]-1 < i {
-                dp[i] = max(dp[i], dp[j]+P[i])
-            }
-        }
+var dp = [Int](repeating: 0, count: N+2) // dp[i] : i일에 얻을 수 있는 최대 수익 (N일에 상담이 끝나는 경우도 고려해서 N+1일까지 확인)
+var maxValue = 0
+for i in 1...N {
+    maxValue = max(maxValue, dp[i])
+    dp[i] = maxValue
+    let nextDay = i + T[i]
+    if nextDay <= N+1 {
+        dp[nextDay] = max(dp[nextDay], dp[i] + P[i])
     }
 }
 
