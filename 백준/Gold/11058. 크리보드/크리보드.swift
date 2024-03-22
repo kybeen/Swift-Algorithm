4f21dc96547d8daa050dd6dcdbaa9e34fee370dc
kybeen <89764127+kybeen@users.noreply.github.com>
2024-03-22 16:36:30 +0900

    [Gold V] Title: 크리보드, Time: 12 ms, Memory: 79508 KB -BaekjoonHub

--- 백준/Gold/11058. 크리보드/크리보드.swift
+++ 백준/Gold/11058. 크리보드/크리보드.swift
@@ -2,7 +2,6 @@ import Foundation
 
 let N = Int(readLine()!)!
 var dp = [Int](repeating: 0, count: 101) // i번 차례에서 출력할 수 있는 A의 최대 개수
-var maxBuffer = [Int](repeating: 0, count: 101) // i번 차례에서 붙여넣을 수 있는 최대 A 개수 (버퍼에 들어 있는 최대 A의 개수)
 
 // 1~6까지는 복붙 필요 x
 for i in 1...6 {
