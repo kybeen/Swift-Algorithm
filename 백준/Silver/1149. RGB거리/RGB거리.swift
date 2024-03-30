e46a2a5e9cd17bc743f3f6f0e502634e9ca261fb
김영빈 (Rei) <89764127+kybeen@users.noreply.github.com>
2024-03-30 15:28:26 +0900

    [Silver I] Title: RGB거리, Time: 12 ms, Memory: 79648 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.swift
+++ 백준/Silver/1149. RGB거리/RGB거리.swift
@@ -1,17 +1,15 @@
 import Foundation
 
-let n = Int(readLine()!)!
-var costs = [[Int]]()
-for _ in 0..<n {
-    costs.append(readLine()!.split(separator: " ").map{ Int($0)! })
+let N = Int(readLine()!)!
+var cost = [[Int]]()
+for _ in 0..<N {
+    cost.append(readLine()!.split(separator: " ").map { Int($0)! })
 }
-
-// dp[i][j] : i번 집은 j색으로 칠할 경우, i번 집까지 색칠하는 최소 비용
-var dp = [[Int]](repeating: [0, 0, 0], count: n)
-dp[0] = costs[0]
-for i in 1..<n {
-    dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + costs[i][0]
-    dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + costs[i][1]
-    dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + costs[i][2]
+var dp = [[Int]](repeating: [Int](repeating: 0, count: 3), count: N)
+dp[0] = cost[0]
+for i in 1...N-1 {
+    dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + cost[i][0]
+    dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + cost[i][1]
+    dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + cost[i][2]
 }
-print(dp[n-1].min()!)
+print(dp[N-1].min()!)
