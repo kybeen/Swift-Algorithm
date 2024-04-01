371e9ada72140b57e4f244c8bc8419eba571baa9
김영빈 (Rei) <89764127+kybeen@users.noreply.github.com>
2024-04-01 09:21:22 +0900

    [Silver II] Title: DFS와 BFS, Time: 28 ms, Memory: 79784 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.swift
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.swift
@@ -1,53 +1,60 @@
 import Foundation
 
-let nums = readLine()!.split(separator: " ").map { Int($0)! }
-let N = nums[0]
-let M = nums[1]
-let V = nums[2]
-var nodes = [[Int]](repeating: [Int](), count: N+1)
+let NMV = readLine()!.split(separator: " ").map { Int($0)! }
+let N = NMV[0]
+let M = NMV[1]
+let V = NMV[2]
+var nodes = Array(repeating: [Int](), count: N+1)
 for _ in 0..<M {
-    let link = readLine()!.split(separator: " ").map { Int($0)! }
-    let a = link[0]
-    let b = link[1]
+    let ab = readLine()!.split(separator: " ").map { Int($0)! }
+    let a = ab[0]
+    let b = ab[1]
     nodes[a].append(b)
     nodes[b].append(a)
 }
-// 작은 수부터 찾기 위해 연결 노드 정렬
-for i in 0..<nodes.count {
-    nodes[i].sort()
-}
-
-func dfs(_ now: Int) {
-    print(now, terminator: " ")
-    visited[now] = true
-    
-    for n in nodes[now] {
-        if !visited[n] {
-            dfs(n)
-        }
-    }
-}
-
-
-func bfs(_ start: Int) {
-    var queue = [Int]()
-    queue.append(start)
-    visited[start] = true
-    
-    while !queue.isEmpty {
-        let now = queue.removeFirst()
-        print(now, terminator: " ")
-        for n in nodes[now] {
-            if !visited[n] {
-                visited[n] = true
-                queue.append(n)
-            }
-        }
-    }
+for i in 1...N {
+    nodes[i].sort(by: <)
 }
 
 var visited = [Bool](repeating: false, count: N+1)
+var dfsResult = ""
+var bfsResult = ""
+func dfs(_ now: Int) {
+    visited[now] = true
+    dfsResult += "\(now) "
+    
+    for node in nodes[now] {
+        if !visited[node] {
+            dfs(node)
+        }
+    }
+}
+
+func bfs(_ start: Int) {
+    visited = [Bool](repeating: false, count: N+1)
+    
+    var idx = 0
+    var queue = [Int]()
+    
+    visited[start] = true
+    queue.append(start)
+    
+    while idx < queue.count {
+        let now = queue[idx]
+        bfsResult += "\(now) "
+        
+        for node in nodes[now] {
+            if !visited[node] {
+                visited[node] = true
+                queue.append(node)
+            }
+        }
+        
+        idx += 1
+    }
+}
+
 dfs(V)
-print()
-visited = [Bool](repeating: false, count: N+1)
 bfs(V)
+print(dfsResult)
+print(bfsResult)
