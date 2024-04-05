4504745e534bfa95724e22a2907811c04acde740
김영빈 (Rei) <89764127+kybeen@users.noreply.github.com>
2024-04-05 17:13:37 +0900

    [Silver I] Title: 포도주 시식, Time: 16 ms, Memory: 79656 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.swift
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.swift
@@ -1,27 +1,24 @@
 import Foundation
 
 let n = Int(readLine()!)!
-var wines = [Int]()
+var grape: [Int] = [0]
 for _ in 0..<n {
-    wines.append(Int(readLine()!)!)
+    grape.append(Int(readLine()!)!)
 }
 
-// dp[i] : i번째 포도주까지 있을 때 마실 수 있는 최대 양
-// dp[i][0] : i번째 포도주를 마시지 않는 경우
-// dp[i][1] : i번째 포도주를 연속 1잔째 마시는 경우
-// dp[i][2] : i번째 포도주를 연속 2잔째 마시는 경우
-var dp = [[Int]](repeating: [Int](repeating: 0, count: 3), count: n)
-dp[0] = [0, wines[0], wines[0]]
+var dp = [Int](repeating: 0, count: 10001)
+dp[1] = grape[1]
 if n > 1 {
-    for i in 1..<n {
-        dp[i][0] = max(dp[i-1][0], dp[i-1][1], dp[i-1][2])
-        dp[i][1] = dp[i-1][0] + wines[i]
-        dp[i][2] = dp[i-1][1] + wines[i]
+    dp[2] = grape[1] + grape[2]
+}
+if n > 2 {
+    for i in 3...n {
+        dp[i] = max(
+            dp[i-1],
+            dp[i-2] + grape[i],
+            dp[i-3] + grape[i-1] + grape[i]
+        )
     }
 }
 
-var result = 0
-for i in 0..<n {
-    result = max(result, dp[i].max()!)
-}
-print(result)
+print(dp[n])
