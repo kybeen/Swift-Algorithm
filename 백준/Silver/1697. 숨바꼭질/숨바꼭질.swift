892d68fc904754008e8b014c0a1cac683bc48ec1
김영빈 (Rei) <89764127+kybeen@users.noreply.github.com>
2024-04-05 15:42:01 +0900

    [Silver I] Title: 숨바꼭질, Time: 20 ms, Memory: 82788 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.swift
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.swift
@@ -4,38 +4,35 @@ let NK = readLine()!.split(separator: " ").map { Int($0)! }
 let N = NK[0]
 let K = NK[1]
 
-var idx = 0
-var queue = [(Int, Int)]() // (현재 위치, 걸린 시간)
 var visited = [Bool](repeating: false, count: 100001)
-
-queue.append((N, 0))
+var queue = [(loc:Int, sec:Int)]()
 visited[N] = true
+queue.append((N, 0))
+
+var idx = 0
 while idx < queue.count {
     let now = queue[idx]
-    
-    // K에 도착하면 종료
-    if now.0 == K {
-        print(now.1)
-        exit(0)
+    if now.loc == K {
+        print(now.sec)
+        break
     }
     
-    // 이동할 수 있는 각 위치
-    let nextA = now.0 - 1
-    let nextB = now.0 + 1
-    let nextC = now.0 * 2
+    let a = now.loc-1
+    let b = now.loc+1
+    let c = now.loc*2
     
-    func goBFS(_ next: Int) {
-        // 0 ~ 100,000의 범위 안에 들어오면서 방문하지 않은 경우에만 진행
-        if 0...100000 ~= next {
-            if !visited[next] {
-                queue.append((next, now.1+1))
-                visited[next] = true
-            }
-        }
+    if 0...100000 ~= a && !visited[a] {
+        visited[a] = true
+        queue.append((a, now.sec+1))
     }
-    goBFS(nextA)
-    goBFS(nextB)
-    goBFS(nextC)
-
+    if 0...100000 ~= b && !visited[b] {
+        visited[b] = true
+        queue.append((b, now.sec+1))
+    }
+    if 0...100000 ~= c && !visited[c] {
+        visited[c] = true
+        queue.append((c, now.sec+1))
+    }
+    
     idx += 1
 }
