83fb7f4b8c231542cdf2ba919d88db15bd697c3f
김영빈 (Rei) <89764127+kybeen@users.noreply.github.com>
2024-04-06 00:42:28 +0900

    [Silver IV] Title: 설탕 배달, Time: 12 ms, Memory: 79508 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.swift
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.swift
@@ -1,16 +1,17 @@
 import Foundation
 
 let N = Int(readLine()!)!
-var dp = [Int](repeating: 5000, count: 5001)
+var dp = [Int](repeating: -1, count: 5001)
 dp[3] = 1
 dp[5] = 1
-for i in 6...5000 {
-    if dp[i-3] != 5000 {
-        dp[i] = min(dp[i], dp[i-3] + 1)
-    }
-    if dp[i-5] != 5000 {
-        dp[i] = min(dp[i], dp[i-5] + 1)
+if N > 5 {
+    for i in 6...N {
+        if dp[i-3] != -1 {
+            dp[i] = dp[i-3] + 1
+        }
+        if dp[i-5] != -1 {
+            dp[i] = dp[i-5] + 1
+        }
     }
 }
-
-print(dp[N] != 5000 ? dp[N] : -1)
+print(dp[N])
