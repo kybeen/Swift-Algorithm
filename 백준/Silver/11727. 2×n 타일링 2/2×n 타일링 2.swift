0072f8e74e95ecfa51e4b4ee46280a61e14eab28
김영빈 (Rei) <89764127+kybeen@users.noreply.github.com>
2024-04-05 15:51:41 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 12 ms, Memory: 79508 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.swift
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.swift
@@ -1,16 +1,12 @@
 import Foundation
 
 let n = Int(readLine()!)!
-if n == 1 {
-    print(1)
-} else if n == 2 {
-    print(3)
-} else {
-    var dp = [Int](repeating: 0, count: n+1)
-    dp[1] = 1
-    dp[2] = 3
+var dp = [Int](repeating: 0, count: 1001)
+dp[1] = 1
+dp[2] = 3
+if n > 2 {
     for i in 3...n {
-        dp[i] = (dp[i-1] + dp[i-2]*2) % 10007
+        dp[i] = (dp[i-1] + (dp[i-2] * 2)) % 10007
     }
-    print(dp[n])
 }
+print(dp[n])
