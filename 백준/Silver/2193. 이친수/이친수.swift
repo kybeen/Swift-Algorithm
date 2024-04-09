3cc9fbf3e8197b76656d659b88f55827c800e45f
김영빈 (Rei) <89764127+kybeen@users.noreply.github.com>
2024-04-09 13:33:14 +0900

    [Silver III] Title: 이친수, Time: 12 ms, Memory: 79508 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.swift
+++ 백준/Silver/2193. 이친수/이친수.swift
@@ -1,15 +1,12 @@
 import Foundation
 
-let n = Int(readLine()!)!
-if n == 1 {
-    print(1)
-} else {
-    // dp[i][j] : 길이가 i일 때 j로 끝나는 이친수의 개수
-    var dp = [[Int]](repeating: [Int](repeating: 0, count: 2), count: n+1)
-    dp[1] = [0, 1]
-    for i in 2...n {
+let N = Int(readLine()!)!
+var dp = Array(repeating: Array(repeating: 0, count: 2), count: 91)
+dp[1] = [0, 1]
+if N > 1 {
+    for i in 2...N {
         dp[i][0] = dp[i-1][0] + dp[i-1][1]
         dp[i][1] = dp[i-1][0]
     }
-    print(dp[n].reduce(0){ $0 + $1 })
 }
+print(dp[N][0] + dp[N][1])
