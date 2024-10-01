5901b79de160c0d6c15b0cce169c3b2ccdabbea5
김영빈 (Rei) <89764127+kybeen@users.noreply.github.com>
2024-10-02 03:24:14 +0900

    [level 3] Title: 베스트앨범, Time: 0.40 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.swift
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.swift
@@ -1,49 +1,43 @@
 import Foundation
 
+struct Song: Hashable {
+    typealias ID = Int
+    
+    let id: Int
+    let playedCount: Int
+}
+
 func solution(_ genres:[String], _ plays:[Int]) -> [Int] {
-    var songs = [String: [(Int, Int)]]() // 장르별 노래들 => [장르 : [(고유번호, 재생횟수)]]
-    var genrePlayCnt = [String: Int]() // 장르별 총 재생 횟수 => [장르 : 재생 횟수]
-    // 딕셔너리 키 세팅
-    for genre in genres {
-        if songs[genre] == nil {
-            songs[genre] = [(Int, Int)]()
-            genrePlayCnt[genre] = 0
+    typealias Genre = String
+    let songNumber = genres.count
+    var musicDict: [Genre: [Song]] = [:]
+    
+    (0..<songNumber).forEach { id in
+        let song = Song(id: id, playedCount: plays[id])
+        let genre = genres[id]
+        if musicDict[genre] != nil {
+            musicDict[genre]!.append(song)
+        } else {
+            musicDict[genre] = [song]
         }
     }
-    // 딕셔너리 값 세팅
-    for i in 0..<plays.count {
-        let genre = genres[i]
-        songs[genre]!.append((i, plays[i]))
-        genrePlayCnt[genre]! += plays[i]
-    }
     
-    var playCnt = [Int]() // 재생 횟수
-    var bestGenres = [String]() // 총 재생 횟수가 많은 순서대로 들어 있는 장르
-    for (_, value) in genrePlayCnt {
-        playCnt.append(value)
-    }
-    // 재생 횟수가 많은 장르부터 bestGenres 배열에 넣어준다.
-    for cnt in playCnt.sorted(by: >) {
-        for (key, value) in genrePlayCnt {
-            if value == cnt {
-                bestGenres.append(key)
+    let bestAlbum = musicDict.sorted {
+        $0.value.reduce(0) { $0 + $1.playedCount } >
+        $1.value.reduce(0) { $0 + $1.playedCount }
+    }.map {
+        $0.value.sorted {
+            if $0.playedCount == $1.playedCount {
+                return $0.id < $1.id
+            } else {
+                return $0.playedCount > $1.playedCount
             }
-        }
+        }.prefix(2)
+    }.map {
+        Array($0)
+    }.reduce([]) {
+        $0 + $1.map({ $0.id })
     }
     
-    var result = [Int]()
-    // 각 장르의 노래들을 많이 재생된 순서로 정렬
-    for (key, value) in songs {
-        songs[key]!.sort { $0.1 > $1.1 }
-    }
-    for bestGenre in bestGenres {
-        // 첫 번째 노래
-        result.append(songs[bestGenre]![0].0)
-        // 장르에 속한 곡이 2곡 이상이면 2번째 곡도 추가
-        if songs[bestGenre]!.count > 1 {
-            result.append(songs[bestGenre]![1].0)
-        }
-    }
-    
-    return result
+    return bestAlbum
 }
