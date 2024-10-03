11798e74fae775bc943964509ad79dbb3750d9d7
김영빈 (Rei) <89764127+kybeen@users.noreply.github.com>
2024-10-04 02:57:42 +0900

    [level 2] Title: H-Index, Time: 54.97 ms, Memory: 16.8 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.swift
+++ 프로그래머스/2/42747. H－Index/H－Index.swift
@@ -1,24 +1,22 @@
 import Foundation
 
 func solution(_ citations:[Int]) -> Int {
-    let citations = citations.sorted(by: >) // 역순 정렬
-    let n = citations.count
-    let maxCnt = citations.first! // 최대 인용 횟수
-    var hIdxArr = [Int](repeating: 0, count: maxCnt+1) // dIdxArr[i] : i번 이상 인용된 논문 개수
+    let sortedCitations = citations.sorted(by: >)
+    let maxCount = sortedCitations.first!
+    var hIndexArr = [Int](repeating: 0, count: maxCount + 1)
+    var hIndex = 0
     
-    for citation in citations {
-        for i in 0...citation {
-            hIdxArr[i] += 1
+    for citation in sortedCitations {
+        for idx in 0...citation {
+            hIndexArr[idx] += 1
         }
     }
     
-    var H_Index = 0
-    for i in 0...maxCnt {
-        // i번 이상 인용된 논문이 i편 이상이면 H-Index 갱신
-        if hIdxArr[i] >= i {
-            H_Index = max(H_Index, i)
+    for h in 0...maxCount {
+        if hIndexArr[h] >= h {
+            hIndex = max(hIndex, h)
         }
     }
     
-    return H_Index
+    return hIndex
 }
