6cc2c202021506fe4ac95f1637fbc917802988d1
김영빈 (Rei) <89764127+kybeen@users.noreply.github.com>
2024-10-13 17:21:31 +0900

    [Gold IV] Title: PPAP, Time: 116 ms, Memory: 119768 KB -BaekjoonHub

---
+++ 백준/Gold/16120. PPAP/PPAP.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+let ppap = Array(readLine()!)
+var ppapStack = [Character]()
+
+ppap.forEach { chr in
+    ppapStack.append(chr)
+    checkPPAP()
+}
+
+if ppapStack.isEmpty || (ppapStack.count == 1 && ppapStack.first! == "P") {
+    print("PPAP")
+} else {
+    print("NP")
+}
+
+func checkPPAP() {
+    guard ppapStack.count >= 4 else { return }
+    let lastIdx = ppapStack.count - 1
+    let isMatched4 = ppapStack[lastIdx] == "P"
+    let isMatched3 = ppapStack[lastIdx - 1] == "A"
+    let isMatched2 = ppapStack[lastIdx - 2] == "P"
+    let isMatched1 = ppapStack[lastIdx - 3] == "P"
+    
+    if isMatched1 && isMatched2 && isMatched3 && isMatched4 {
+        ppapStack.removeLast(4)
+        ppapStack.append("P")
+    }
+}
