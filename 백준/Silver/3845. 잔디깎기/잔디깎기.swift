f24ebcbc364df465668f7f358ce7fc432ec0a921
김영빈 (Rei) <89764127+kybeen@users.noreply.github.com>
2024-11-04 10:48:41 +0900

    [Silver V] Title: 잔디깎기, Time: 24 ms, Memory: 79516 KB -BaekjoonHub

--- 백준/Silver/3845. 잔디깎기/잔디깎기.swift
+++ 백준/Silver/3845. 잔디깎기/잔디깎기.swift
@@ -1,43 +1,35 @@
 import Foundation
 
 testCase: while true {
-    let firstInput = readLine()!.split(separator: " ")
-    let nx = Int(firstInput[0])!
-    let ny = Int(firstInput[1])!
-    let w = Double(firstInput[2])!
-    let wHalf = w / 2
+    let firstLine = readLine()!.split(separator: " ")
+    let nx = Int(firstLine[0])!
+    let ny = Int(firstLine[1])!
+    let w = Double(firstLine[2])!
     guard !(nx == 0 && ny == 0 && w == 0.0) else { exit(0) }
 
     let x = readLine()!.split(separator: " ").map({ Double($0)! }).sorted(by: <)
     let y = readLine()!.split(separator: " ").map({ Double($0)! }).sorted(by: <)
     
-    var left: Double = 0.0
-    var up: Double = 0.0
-    for i in 0..<nx {
-        guard x[i] - wHalf <= left else {
-            print("NO")
-            continue testCase
-        }
-        if i == nx-1 {
-            guard x[i] + wHalf >= 75.0 else {
-                print("NO")
-                continue testCase
-            }
-        }
-        left = x[i] + wHalf
-    }
-    for i in 0..<ny {
-        guard y[i] - wHalf <= up else {
-            print("NO")
-            continue testCase
-        }
-        if i == ny-1 {
-            guard y[i] + wHalf >= 100.0 else {
-                print("NO")
-                continue testCase
-            }
-        }
-        up = y[i] + wHalf
-    }
+    if !isJandiLineCrashed(pointArr: x, width: w, totalLength: 75.0) { continue testCase }
+    if !isJandiLineCrashed(pointArr: y, width: w, totalLength: 100.0) { continue testCase }
     print("YES")
+}
+
+func isJandiLineCrashed(pointArr: [Double], width: Double, totalLength: Double) -> Bool {
+    let half = width / 2
+    var start: Double = 0.0
+    for i in 0..<pointArr.count {
+        guard pointArr[i] - half <= start else {
+            print("NO")
+            return false
+        }
+        if i == pointArr.count-1 {
+            guard pointArr[i] + half >= totalLength else {
+                print("NO")
+                return false
+            }
+        }
+        start = pointArr[i] + half
+    }
+    return true
 }
