4410dc5993bab52c7236ef50ef0b22ec0375717e
김영빈 (Rei) <89764127+kybeen@users.noreply.github.com>
2024-12-17 02:17:20 +0900

    [level 1] Title: 모의고사, Time: 0.75 ms, Memory: 17 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=swift) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
 
 ### 성능 요약
 
-메모리: 16.7 MB, 시간: 0.72 ms
+메모리: 17 MB, 시간: 0.75 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 4일 17:10:57
+2024년 12월 17일 02:17:18
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.swift
+++ 프로그래머스/1/42840. 모의고사/모의고사.swift
@@ -1,28 +1,70 @@
+// import Foundation
+
+// func solution(_ answers:[Int]) -> [Int] {
+//     let a = [1, 2, 3, 4, 5]
+//     let b = [2, 1, 2, 3, 2, 4, 2, 5]
+//     let c = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+//     let aCnt = a.count
+//     let bCnt = b.count
+//     let cCnt = c.count
+    
+//     var score = [0, 0, 0]
+//     var result = [Int]()
+    
+//     for i in 0..<answers.count {
+//         if answers[i] == a[i % aCnt] { score[0] += 1 }
+//         if answers[i] == b[i % bCnt] { score[1] += 1 }
+//         if answers[i] == c[i % cCnt] { score[2] += 1 }
+//     }
+    
+//     let highScore = score.max()!
+//     for i in 0..<3 {
+//         if score[i] == highScore {
+//             result.append(i+1)
+//         }
+//     }
+    
+//     return result
+// }
+
+
 import Foundation
 
 func solution(_ answers:[Int]) -> [Int] {
-    let a = [1, 2, 3, 4, 5]
-    let b = [2, 1, 2, 3, 2, 4, 2, 5]
-    let c = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-    let aCnt = a.count
-    let bCnt = b.count
-    let cCnt = c.count
-    
+    let supoOne = [1, 2, 3, 4, 5]
+    let supoTwo = [2, 1, 2, 3, 2, 4, 2, 5]
+    let supoThree = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
     var score = [0, 0, 0]
-    var result = [Int]()
     
-    for i in 0..<answers.count {
-        if answers[i] == a[i % aCnt] { score[0] += 1 }
-        if answers[i] == b[i % bCnt] { score[1] += 1 }
-        if answers[i] == c[i % cCnt] { score[2] += 1 }
+    for idx in 0..<answers.count {
+        let answer = answers[idx]
+        if answer == supoOne[idx % supoOne.count] { score[0] += 1 }
+        if answer == supoTwo[idx % supoTwo.count] { score[1] += 1 }
+        if answer == supoThree[idx % supoThree.count] { score[2] += 1 }
     }
     
-    let highScore = score.max()!
-    for i in 0..<3 {
-        if score[i] == highScore {
-            result.append(i+1)
+    var result = [Int]()
+    let highest = score.max()!
+    for num in 1...3 {
+        if score[num - 1] == highest {
+            result.append(num)
         }
     }
-    
-    return result
+    return result.sorted(by: <)
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
