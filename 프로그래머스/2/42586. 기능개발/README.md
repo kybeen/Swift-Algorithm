9ce105460e242f210c651b5f2fd2417bb47d0b4f
김영빈 (Rei) <89764127+kybeen@users.noreply.github.com>
2024-12-23 15:26:57 +0900

    [level 2] Title: 기능개발, Time: 0.24 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=swift) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
 
 ### 성능 요약
 
-메모리: 16.5 MB, 시간: 0.02 ms
+메모리: 16.5 MB, 시간: 0.24 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 11일 17:43:33
+2024년 12월 23일 15:26:55
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.swift
+++ 프로그래머스/2/42586. 기능개발/기능개발.swift
@@ -1,28 +1,62 @@
+// import Foundation
+
+// func solution(_ progresses:[Int], _ speeds:[Int]) -> [Int] {
+//     var nowProgresses = progresses
+//     var releases = [Int]()
+    
+//     var idx = 0
+//     while idx < nowProgresses.count {
+//         // 오늘의 작업 수행
+//         for i in idx..<nowProgresses.count {
+//             if nowProgresses[i] < 100 {
+//                 nowProgresses[i] += speeds[i]
+//             }
+//         }
+        
+//         // 오늘 가능한 배포 개수 확인 후 반영
+//         if nowProgresses[idx] >= 100 {
+//             var todayRelease = 0
+//             while idx < nowProgresses.count && nowProgresses[idx] >= 100 {
+//                 todayRelease += 1
+//                 idx += 1
+//             }
+//             releases.append(todayRelease)
+//         }
+//     }
+    
+//     return releases
+// }
+
+
 import Foundation
 
 func solution(_ progresses:[Int], _ speeds:[Int]) -> [Int] {
-    var nowProgresses = progresses
-    var releases = [Int]()
+    var taskStack = Array(progresses.reversed())
+    var reversedSpeeds = Array(speeds.reversed())
     
-    var idx = 0
-    while idx < nowProgresses.count {
-        // 오늘의 작업 수행
-        for i in idx..<nowProgresses.count {
-            if nowProgresses[i] < 100 {
-                nowProgresses[i] += speeds[i]
-            }
+    var result = [Int]()
+    while !taskStack.isEmpty {
+        var releaseCount = 0
+        for i in 0..<taskStack.count {
+            taskStack[i] += reversedSpeeds[i]
         }
-        
-        // 오늘 가능한 배포 개수 확인 후 반영
-        if nowProgresses[idx] >= 100 {
-            var todayRelease = 0
-            while idx < nowProgresses.count && nowProgresses[idx] >= 100 {
-                todayRelease += 1
-                idx += 1
-            }
-            releases.append(todayRelease)
+        while let last = taskStack.last, last >= 100 {
+            _ = taskStack.popLast()
+            _ = reversedSpeeds.popLast()
+            releaseCount += 1
         }
+        result.append(releaseCount)
     }
     
-    return releases
+    return result.filter { $0 != 0 }
 }
+
+
+
+
+
+
+
+
+
+
