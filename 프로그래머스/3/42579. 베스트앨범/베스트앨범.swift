e63a9a5fdc01d8a61304ec5ab2471aead403f54c
김영빈 (Rei) <89764127+kybeen@users.noreply.github.com>
2024-12-26 16:53:32 +0900

    [level 3] Title: 베스트앨범, Time: 0.65 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.swift
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.swift
@@ -1,43 +1,58 @@
 import Foundation
 
-struct Song: Hashable {
-    typealias ID = Int
-    
+// 1. 재생 많은 장르
+// 2. 재생 많은 노래
+// 3. 고유번호 낮은 노래
+struct Music {
     let id: Int
     let playedCount: Int
 }
 
+struct GenreInfo {
+    var musics: [Music]
+    var totalPlayedCount: Int {
+        musics.reduce(0) { $0 + $1.playedCount }
+    }
+}
+
 func solution(_ genres:[String], _ plays:[Int]) -> [Int] {
-    typealias Genre = String
-    let songNumber = genres.count
-    var musicDict: [Genre: [Song]] = [:]
+    var genreDict = [String: GenreInfo]()
+    let musicCount = genres.count
     
-    (0..<songNumber).forEach { id in
-        let song = Song(id: id, playedCount: plays[id])
+    for id in 0..<musicCount {
         let genre = genres[id]
-        if musicDict[genre] != nil {
-            musicDict[genre]!.append(song)
+        let music = Music(id: id, playedCount: plays[id])
+        
+        if genreDict[genre] != nil {
+            genreDict[genre]!.musics.append(music)
         } else {
-            musicDict[genre] = [song]
+            genreDict[genre] = GenreInfo(musics: [music])
         }
     }
     
-    let bestAlbum = musicDict
-    .sorted {
-        $0.value.reduce(0) { $0 + $1.playedCount } >
-        $1.value.reduce(0) { $0 + $1.playedCount }
-    }.map {
-        $0.value.sorted {
+    var bestAlbum = [Int]()
+    
+    // 재생 횟수가 많은 장르
+    let topGenres = genreDict
+    .sorted {$0.value.totalPlayedCount > $1.value.totalPlayedCount }
+    .map(\.key)
+    
+    for genre in topGenres {
+        // 각 장르 별 재생 횟수가 많은 노래들
+        let sortedMusics = genreDict[genre]!.musics.sorted {
             if $0.playedCount == $1.playedCount {
                 return $0.id < $1.id
             } else {
                 return $0.playedCount > $1.playedCount
             }
-        }.prefix(2)
-    }.map {
-        Array($0)
-    }.reduce([]) {
-        $0 + $1.map({ $0.id })
+        }
+        // 장르 별 최대 2곡까지 수록
+        if let top = sortedMusics.first {
+            bestAlbum.append(top.id)
+        }
+        if sortedMusics.count > 1 {
+            bestAlbum.append(sortedMusics[1].id)
+        }
     }
     
     return bestAlbum
