7edd80001c0ec3a32ecdba3adebbacfd9aad3af6
김영빈 (Rei) <89764127+kybeen@users.noreply.github.com>
2025-01-02 10:46:21 +0900

    [level 3] Title: 양과 늑대, Time: 14.32 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.swift
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.swift
@@ -1,10 +1,11 @@
 import Foundation
 
+// BFS 노드 정보
 struct Node {
-    let num: Int
+    let num: Int // 노드 번호
     let sheep: Int
     let wolf: Int
-    var nextNodes: Set<Int>
+    var nextNodes: Set<Int> // 현재 이동 상태를 기준으로 방문 가능한 다음 노드들
     
     var sheepCount: Int {
         return sheep - wolf
@@ -21,6 +22,7 @@ struct Node {
 func solution(_ info:[Int], _ edges:[[Int]]) -> Int {
     var tree = Array(repeating: [Int](), count: info.count)
     
+    // 트리 생성
     for edge in edges {
         let parent = edge[0]
         let child = edge[1]
@@ -30,7 +32,7 @@ func solution(_ info:[Int], _ edges:[[Int]]) -> Int {
     var result = 1
     var idx = 0
     var queue = [Node]()
-    let root = Node(0, sheep: 1, wolf: 0, nextNodes: Set<Int>(tree[0]))
+    let root = Node(0, sheep: 1, wolf: 0, nextNodes: Set<Int>(tree[0])) // 루트 노드
     queue.append(root)
     
     while idx < queue.count {
@@ -38,10 +40,11 @@ func solution(_ info:[Int], _ edges:[[Int]]) -> Int {
         result = max(result, now.sheep)
         
         for next in now.nextNodes {
+            // 큐에 새로 넣어줄 노드는 next를 방문한 상태이기 때문에 nextNodes에서 next를 제외해줘야 한다.
             var newNextNodes = now.nextNodes
             newNextNodes.remove(next)
             for child in tree[next] {
-                newNextNodes.insert(child)
+                newNextNodes.insert(child) // next의 자식 노드들을 nextNodes에 추가
             }
             
             if info[next] == 0 { // 양
