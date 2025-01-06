5e09123d49a94f66528f720962212a2568516997
김영빈 (Rei) <89764127+kybeen@users.noreply.github.com>
2025-01-06 19:26:50 +0900

    [level 2] Title: 피로도, Time: 27.20 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/README.md
+++ 프로그래머스/2/87946. 피로도/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피로도 - 87946 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946?language=swift) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946) 
 
 ### 성능 요약
 
-메모리: 16.4 MB, 시간: 0.05 ms
+메모리: 16.3 MB, 시간: 27.20 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 1일 15:58:55
+2025년 01월 06일 19:26:47
 
 ### 문제 설명
 
--- 프로그래머스/2/87946. 피로도/피로도.swift
+++ 프로그래머스/2/87946. 피로도/피로도.swift
@@ -1,25 +1,72 @@
+// import Foundation
+
+// func solution(_ k:Int, _ dungeons:[[Int]]) -> Int {
+//     let n = dungeons.count
+//     var visited = [Bool](repeating: false, count: n)
+//     var result = -1
+    
+//     // rest: 잔여 피로도, depth: 탐험한 던전 수
+//     func dfs(_ rest: Int, _ depth: Int) {
+//         // 최대 탐험 던전 수 갱신
+//         result = max(result, depth)
+        
+//         for i in 0..<n {
+//             // 방문하지 않았고 잔여 피로도가 최소 필요 피로도 이상이면 던전 탐험
+//             if !visited[i] && dungeons[i][0] <= rest {
+//                 visited[i] = true
+//                 dfs(rest - dungeons[i][1], depth+1)
+//                 visited[i] = false
+//             }
+//         }
+//     }
+    
+//     dfs(k, 0)
+    
+//     return result
+// }
+
 import Foundation
 
 func solution(_ k:Int, _ dungeons:[[Int]]) -> Int {
-    let n = dungeons.count
-    var visited = [Bool](repeating: false, count: n)
-    var result = -1
+    var visited: [Bool] = Array(repeating: false, count: dungeons.count)
+    var result: Int = 0
     
-    // rest: 잔여 피로도, depth: 탐험한 던전 수
-    func dfs(_ rest: Int, _ depth: Int) {
+    func dfs(_ depth: Int, _ fatigue: Int) {
         result = max(result, depth)
         
-        for i in 0..<n {
-            // 방문하지 않았고 잔여 피로도가 최소 필요 피로도 이상이면 던전 탐험
-            if !visited[i] && dungeons[i][0] <= rest {
+        for i in 0..<dungeons.count {
+            let dungeon = dungeons[i]
+            if !visited[i] && (fatigue >= dungeon[0]) {
                 visited[i] = true
-                dfs(rest - dungeons[i][1], depth+1)
+                dfs(depth+1, fatigue-dungeon[1])
                 visited[i] = false
             }
         }
     }
     
-    dfs(k, 0)
+    dfs(0, k)
     
     return result
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
