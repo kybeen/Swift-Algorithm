2aeff539e47508aaca0681c39b3cba1336dc2961
김영빈 (Rei) <89764127+kybeen@users.noreply.github.com>
2025-01-09 18:25:39 +0900

    [level 1] Title: K번째수, Time: 0.14 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.swift
+++ 프로그래머스/1/42748. K번째수/K번째수.swift
@@ -1,49 +1,15 @@
-// import Foundation
-
-// func solution(_ array:[Int], _ commands:[[Int]]) -> [Int] {
-//     var result = [Int]()
-    
-//     for command in commands {
-//         let i = command[0] - 1
-//         let j = command[1] - 1
-//         let k = command[2] - 1
-        
-//         let sliceArray = array[i...j].sorted()
-//         result.append(sliceArray[k])
-//     }
-    
-    
-//     return result
-// }
-
 import Foundation
 
 func solution(_ array:[Int], _ commands:[[Int]]) -> [Int] {
     var result = [Int]()
     
     for command in commands {
-        let i = command[0]
-        let j = command[1]
-        let k = command[2]
+        let i = command[0] - 1
+        let j = command[1] - 1
+        let k = command[2] - 1
         
-        var arr = [Int]()
-        for idx in i-1...j-1 {
-            arr.append(array[idx])
-        }
-        let resultVal = arr.sorted(by: <)[k-1]
-        result.append(resultVal)
+        result.append(array[i...j].sorted(by: <)[k])
     }
     
     return result
 }
-
-
-
-
-
-
-
-
-
-
-
