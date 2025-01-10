4d24d57c47088210462f4c34ce04b484a101e2a8
김영빈 (Rei) <89764127+kybeen@users.noreply.github.com>
2025-01-10 18:19:35 +0900

    [level 2] Title: 배달, Time: 1.38 ms, Memory: 17.1 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.swift
+++ 프로그래머스/2/12978. 배달/배달.swift
@@ -1,15 +1,16 @@
 import Foundation
 
 func solution(_ N:Int, _ road:[[Int]], _ k:Int) -> Int {
-    var tree: [[Int]] = Array(repeating: Array(repeating: Int.max, count: N+1), count: N+1)
-    var costs: [Int] = Array(repeating: Int.max, count: N+1)
+    var edges: [[Int]] = Array(repeating: Array(repeating: Int.max, count: N+1), count: N+1) // 마을 간 도로 비용
+    var costs: [Int] = Array(repeating: Int.max, count: N+1) // 1번 마을에서 각 마을까지의 최소 비용
     
     for r in road {
         let a = r[0]
         let b = r[1]
         let cost = r[2]
-        tree[a][b] = min(tree[a][b], cost)
-        tree[b][a] = min(tree[b][a], cost)
+        // 도로가 여러개인 경우, 최소값만 저장
+        edges[a][b] = min(edges[a][b], cost)
+        edges[b][a] = min(edges[b][a], cost)
     }
     
     var idx = 0
@@ -19,11 +20,11 @@ func solution(_ N:Int, _ road:[[Int]], _ k:Int) -> Int {
     while idx < queue.count {
         let now = queue[idx]
         
-        // 인접 노드들의 최단거리 갱신
         for next in 1...N {
-            if tree[now.node][next] == Int.max { continue }
+            // 현재 노드에서 인접 노드(방문 가능한 노드)로의 최단거리 갱신
+            guard edges[now.node][next] != Int.max else { continue }
             
-            let nextCost = now.cost + tree[now.node][next]
+            let nextCost = now.cost + edges[now.node][next]
             if nextCost < costs[next] {
                 costs[next] = nextCost
                 queue.append((next, nextCost))
