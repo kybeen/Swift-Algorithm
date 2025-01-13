f989bc9cfcc5b806a015b87bcdf80a12d0882c1d
김영빈 (Rei) <89764127+kybeen@users.noreply.github.com>
2025-01-13 16:31:57 +0900

    [level 2] Title: 카펫, Time: 0.08 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.swift
+++ 프로그래머스/2/42842. 카펫/카펫.swift
@@ -1,27 +1,20 @@
 import Foundation
 
 func solution(_ brown:Int, _ yellow:Int) -> [Int] {
-    let totalGrid = brown + yellow // 전체 격자 개수
-    var result = [Int]()
+    let area = brown + yellow // 가로x세로 == brown+yellow == 넓이
+    let maxHeight = Int(sqrt(Double(area)))
+    var result = [0, 0]
     
-    // 가로-세로 중 한 쪽의 길이 (문제 조건 상 작은 쪽의 길이는 최소 3 이상이어야 함)
-    var a = 3
-    
-    while true {
-        // 가능한 가로-세로 조합의 경우를 확인
-        guard totalGrid % a == 0 else {
-            a += 1
-            continue
+    // 세로는 항상 가로 이하이기 때문에 넓이의 제곱근까지만 확인
+    for height in 3...maxHeight {
+        let yellowHeight = height - 2
+        let yellowWidth = yellow / (yellowHeight)
+        let yellowCount = yellowHeight * yellowWidth
+        let brownCount = (yellowHeight*2) + (yellowWidth*2) + 4
+        if yellowCount == yellow && brownCount == brown {
+            result = [area / height, height]
         }
-        let b = totalGrid / a
-        // 갈색 격자의 수 조건(그림 참고) : (a + (b-2)) * 2
-        if (a + b - 2) * 2 == brown {
-            result = [a, b]
-            break
-        }
-        
-        a += 1
     }
     
-    return result.sorted(by: >) // 내림차순 정렬해서 리턴
+    return result
 }
