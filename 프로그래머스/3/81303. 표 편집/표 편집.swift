35282eb64af0feba72d094fba455efa185c562d7
김영빈 (Rei) <89764127+kybeen@users.noreply.github.com>
2025-01-29 13:26:14 +0900

    [level 3] Title: 표 편집, Time: 937.55 ms, Memory: 93.3 MB -BaekjoonHub

--- 프로그래머스/3/81303. 표 편집/표 편집.swift
+++ 프로그래머스/3/81303. 표 편집/표 편집.swift
@@ -1,73 +1,68 @@
 import Foundation
 
 class Node {
+    let idx: Int
     var prev: Node?
     var next: Node?
-    var idx: Int
     
-    init(prev: Node?, next: Node?, idx: Int) {
+    init(_ idx: Int, _ prev: Node? = nil, _ next: Node? = nil) {
+        self.idx = idx
         self.prev = prev
         self.next = next
-        self.idx = idx
     }
 }
 
 func solution(_ n:Int, _ k:Int, _ cmd:[String]) -> String {
-    var result = Array(repeating: "O", count: n)
-    var nodes = [Node]()
+    var result: [String] = Array(repeating: "O", count: n)
+    var table = [Node]()
     for i in 0..<n {
-        let node = Node(prev: nil, next: nil, idx: i)
-        nodes.append(node)
+        let node = Node(i, table.last)
+        table.last?.next = node
+        table.append(node)
     }
     
-    nodes[0].next = nodes[1]
-    for i in 1..<n-1 {
-        nodes[i].prev = nodes[i-1]
-        nodes[i].next = nodes[i+1]
-    }
-    nodes[n-1].prev = nodes[n-2]
-    
-    // for node in nodes {
-    //     print("Node\(node.idx) => prev: \(node.prev?.idx), next: \(node.next?.idx)")
+    // for t in table {
+    //     print("\(t.idx)번 => prev:\(t.prev?.idx ?? -1) / next:\(t.next?.idx ?? -1)")
     // }
     
+    var now = table[k]
     var deleted = [Node]()
-    var now = nodes[k]
     
     for command in cmd {
-        if command == "C" {
+        let command = command.components(separatedBy: " ")
+        if command[0] == "U" {
+            var count = Int(command[1])!
+            while count > 0 {
+                if let prev = now.prev {
+                    now = prev
+                }
+                count -= 1
+            }
+        } else if command[0] == "D" {
+            var count = Int(command[1])!
+            while count > 0 {
+                if let next = now.next {
+                    now = next
+                }
+                count -= 1
+            }
+        } else if command[0] == "C" {
             result[now.idx] = "X"
-            deleted.append(now)
-            
             now.prev?.next = now.next
             now.next?.prev = now.prev
-            if now.next == nil {
-                now = now.prev!
-            } else {
-                now = now.next!
+            deleted.append(now)
+            if let next = now.next {
+                now = next
+            } else if let prev = now.prev {
+                now = prev
             }
-            
-        } else if command == "Z" {
-            let popped = deleted.popLast()!
-            result[popped.idx] = "O"
-            popped.prev?.next = popped
-            popped.next?.prev = popped
-            
-        } else {
-            let commandPair = command.split(separator: " ")
-            let direction = commandPair[0]
-            let offset = Int(commandPair[1])!
-            if direction == "U" {
-                for _ in 0..<offset {
-                    now = now.prev!
-                }
-            } else if direction == "D" {
-                for _ in 0..<offset {
-                    now = now.next!
-                }
-            }
+        } else if command[0] == "Z" {
+            let restored = deleted.popLast()!
+            restored.prev?.next = restored
+            restored.next?.prev = restored
+            result[restored.idx] = "O"
         }
     }
     
-    return result.joined(separator: "")
+    return result.joined()
 }
