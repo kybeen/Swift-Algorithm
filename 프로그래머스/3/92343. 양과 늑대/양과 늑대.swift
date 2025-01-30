895a1edff9f4e7c03ddc83ef2e8ecb1e7a0225d0
김영빈 (Rei) <89764127+kybeen@users.noreply.github.com>
2025-01-30 15:13:41 +0900

    [level 3] Title: 양과 늑대, Time: 28.34 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.swift
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.swift
@@ -1,71 +1,28 @@
 import Foundation
 
-// BFS 노드 정보
-struct Node {
-    let num: Int // 노드 번호
-    let sheep: Int
-    let wolf: Int
-    var nextNodes: Set<Int> // 현재 이동 상태를 기준으로 방문 가능한 다음 노드들
-    
-    var sheepCount: Int {
-        return sheep - wolf
-    }
-    
-    init(_ num: Int, sheep: Int, wolf: Int, nextNodes: Set<Int> = []) {
-        self.num = num
-        self.sheep = sheep
-        self.wolf = wolf
-        self.nextNodes = nextNodes
-    }
-}
-
 func solution(_ info:[Int], _ edges:[[Int]]) -> Int {
-    var tree = Array(repeating: [Int](), count: info.count)
-    
-    // 트리 생성
+    var childs = Array(repeating: [Int](), count: info.count) // 자식 노드 배열
     for edge in edges {
-        let parent = edge[0]
-        let child = edge[1]
-        tree[parent].append(child)
+        let (parent, child) = (edge[0], edge[1])
+        childs[parent].append(child)
     }
     
-    var result = 1
-    var idx = 0
-    var queue = [Node]()
-    let root = Node(0, sheep: 1, wolf: 0, nextNodes: Set<Int>(tree[0])) // 루트 노드
-    queue.append(root)
-    
-    while idx < queue.count {
-        let now = queue[idx]
-        result = max(result, now.sheep)
+    var result = 0
+    func dfs(_ now: Int, _ sheep: Int, _ wolf: Int, _ nextNodes: [Int]) {
+        guard sheep > wolf else { return }
+        result = max(result, sheep)
         
-        for next in now.nextNodes {
-            // 큐에 새로 넣어줄 노드는 next를 방문한 상태이기 때문에 nextNodes에서 next를 제외해줘야 한다.
-            var newNextNodes = now.nextNodes
-            newNextNodes.remove(next)
-            for child in tree[next] {
-                newNextNodes.insert(child) // next의 자식 노드들을 nextNodes에 추가
-            }
-            
-            if info[next] == 0 { // 양
-                let nextNode = Node(next,
-                                    sheep: now.sheep+1,
-                                    wolf: now.wolf,
-                                    nextNodes: newNextNodes)
-                queue.append(nextNode)
-            } else { // 늑대
-                if now.sheepCount > 1 {
-                    let nextNode = Node(next,
-                                        sheep: now.sheep,
-                                        wolf: now.wolf+1,
-                                        nextNodes: newNextNodes)
-                    queue.append(nextNode)
-                }
+        for next in nextNodes {
+            let nextNextNodes = nextNodes.filter({ $0 != next }) + childs[next]
+            if info[next] == 0 {
+                dfs(next, sheep+1, wolf, nextNextNodes)
+            } else {
+                dfs(next, sheep, wolf+1, nextNextNodes)
             }
         }
-        
-        idx += 1
     }
+    
+    dfs(0, 1, 0, childs[0])
     
     return result
 }
