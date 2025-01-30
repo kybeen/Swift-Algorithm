de4c42d3d69b787c80daa455c1e9116738cdcce9
김영빈 (Rei) <89764127+kybeen@users.noreply.github.com>
2025-01-30 15:58:18 +0900

    [level 3] Title: 길 찾기 게임, Time: 162.37 ms, Memory: 20.8 MB -BaekjoonHub

--- 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.swift
+++ 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.swift
@@ -2,35 +2,29 @@ import Foundation
 
 class Node {
     let num: Int
-    let x: Int
-    let y: Int
+    let point: (x: Int, y: Int)
     var left: Node?
     var right: Node?
     
-    var description: String {
-        "\(num)번 노드 => left: \(left?.num ?? -1) / right: \(right?.num ?? -1)"
-    }
-    
-    init(_ num: Int, x: Int, y: Int) {
+    init(_ num: Int, _ point: (x: Int, y: Int)) {
         self.num = num
-        self.x = x
-        self.y = y
+        self.point = point
         self.left = nil
         self.right = nil
     }
     
-    func addChild(_ node: Node) {
-        if node.x < self.x { // 왼쪽 자식
-            if left != nil {
-                left?.addChild(node)
+    func addChild(_ child: Node) {
+        if child.point.x < point.x {
+            if left == nil {
+                left = child
             } else {
-                left = node
+                left?.addChild(child)
             }
-        } else { // 오른쪽 자식
-            if right != nil {
-                right?.addChild(node)
+        } else {
+            if right == nil {
+                right = child
             } else {
-                right = node
+                right?.addChild(child)
             }
         }
     }
@@ -39,44 +33,37 @@ class Node {
 func solution(_ nodeinfo:[[Int]]) -> [[Int]] {
     var nodes = [Node]()
     for i in 0..<nodeinfo.count {
-        let node = Node(i+1, x: nodeinfo[i][0], y: nodeinfo[i][1])
-        nodes.append(node)
+        let info = nodeinfo[i]
+        let point = (info[0], info[1])
+        nodes.append(Node(i+1, point))
+    }
+    nodes.sort { $0.point.y > $1.point.y } // 레벨이 높은 순서로 정렬
+    let root = nodes.first!
+    
+    for i in 1..<nodes.count {
+        root.addChild(nodes[i])
     }
     
-    // y좌표가 높은 순으로 정렬 => first가 루트가 된다.
-    let sortedNodes = nodes.sorted { $0.y > $1.y }
-    
-    // 이진 트리 구성
-    let root = sortedNodes.first!
-    for i in 1..<sortedNodes.count {
-        let node = sortedNodes[i]
-        root.addChild(node)
+    var stk = [Int]()
+    func traversal(_ node: Node, isPostOrder: Bool = false) {
+        if !isPostOrder { stk.append(node.num) }
+        
+        if let left = node.left {
+            traversal(left, isPostOrder: isPostOrder)
+        }
+        if let right = node.right {
+            traversal(right, isPostOrder: isPostOrder)
+        }
+        
+        if isPostOrder { stk.append(node.num) }
     }
     
-    var result: [[Int]] = [[], []]
-    // 전위 순회
-    func preTraversal(_ now: Node) {
-        result[0].append(now.num)
-        if let left = now.left {
-            preTraversal(left)
-        }
-        if let right = now.right {
-            preTraversal(right)
-        }
-    }
-    // 후위 순회
-    func postTraversal(_ now: Node) {
-        if let left = now.left {
-            postTraversal(left)
-        }
-        if let right = now.right {
-            postTraversal(right)
-        }
-        result[1].append(now.num)
-    }
-    
-    preTraversal(root)
-    postTraversal(root)
+    var result = [[Int]]()
+    traversal(root)
+    result.append(stk)
+    stk = []
+    traversal(root, isPostOrder: true)
+    result.append(stk)
     
     return result
 }
