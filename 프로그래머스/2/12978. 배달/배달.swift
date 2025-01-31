826fb6016a12b4cfd2de37311c332ab05b38e276
김영빈 (Rei) <89764127+kybeen@users.noreply.github.com>
2025-01-31 14:53:20 +0900

    [level 2] Title: 배달, Time: 7.51 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.swift
+++ 프로그래머스/2/12978. 배달/배달.swift
@@ -1,32 +1,26 @@
 import Foundation
 
 func solution(_ N:Int, _ road:[[Int]], _ k:Int) -> Int {
-    var edges: [[Int]] = Array(repeating: Array(repeating: Int.max, count: N+1), count: N+1) // 마을 간 도로 비용
-    var costs: [Int] = Array(repeating: Int.max, count: N+1) // 1번 마을에서 각 마을까지의 최소 비용
-    
+    var roades = Array(repeating: Array(repeating: Int.max, count: N+1), count: N+1)
+    var cost = Array(repeating: Int.max, count: N+1) // cost[i]: 1번 마을에서 i번 마을까지 최소 시간
     for r in road {
-        let a = r[0]
-        let b = r[1]
-        let cost = r[2]
-        // 도로가 여러개인 경우, 최소값만 저장
-        edges[a][b] = min(edges[a][b], cost)
-        edges[b][a] = min(edges[b][a], cost)
+        let (a, b, time) = (r[0], r[1], r[2])
+        roades[a][b] = min(roades[a][b], time)
+        roades[b][a] = min(roades[b][a], time)
     }
     
     var idx = 0
-    var queue: [(node: Int, cost: Int)] = [(1, 0)]
-    costs[1] = 0
+    var queue = [(town: Int, time: Int)]()
     
+    queue.append((1, 0))
     while idx < queue.count {
         let now = queue[idx]
+        cost[now.town] = min(cost[now.town], now.time)
         
         for next in 1...N {
-            // 현재 노드에서 인접 노드(방문 가능한 노드)로의 최단거리 갱신
-            guard edges[now.node][next] != Int.max else { continue }
-            
-            let nextCost = now.cost + edges[now.node][next]
-            if nextCost < costs[next] {
-                costs[next] = nextCost
+            guard roades[now.town][next] != Int.max else { continue }
+            let nextCost = now.time + roades[now.town][next]
+            if nextCost <= cost[next] {
                 queue.append((next, nextCost))
             }
         }
@@ -34,5 +28,5 @@ func solution(_ N:Int, _ road:[[Int]], _ k:Int) -> Int {
         idx += 1
     }
 
-    return costs.filter { $0 <= k }.count
+    return cost.filter { $0 <= k }.count
 }
