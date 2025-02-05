b43165fff2d16e0c7b5c38bba0820af1106806ca
김영빈 (Rei) <89764127+kybeen@users.noreply.github.com>
2025-02-05 12:45:30 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 496 ms, Memory: 87248 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.swift
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.swift
@@ -1,25 +1,30 @@
 let NM = readLine()!.split(separator: " ").map { Int($0)! }
-let N = NM[0]
-let M = NM[1]
-
-var table: [[Int]] = [[Int](repeating: 0, count: N+1)]
+let (N, M) = (NM[0], NM[1])
+var table = [[Int]]()
 for _ in 0..<N {
-    table.append([0] + readLine()!.split(separator: " ").map { Int($0)! })
+    let row = readLine()!.split(separator: " ").map { Int($0)! }
+    table.append(row)
 }
 
-// (1,1) ~ (i,j)의 합으로 테이블 채우기
-for i in 1...N {
-    for j in 1...N {
-        table[i][j] += table[i - 1][j] + table[i][j - 1] - table[i - 1][j - 1]
+// 누적합 테이블 생성
+var dp = Array(repeating: Array(repeating: 0, count: N+1), count: N+1)
+dp[1][1] = table[0][0]
+for i in 2...N {
+    dp[1][i] = dp[1][i-1] + table[0][i-1]
+    dp[i][1] = dp[i-1][1] + table[i-1][0]
+}
+for i in 2...N {
+    for j in 2...N {
+        dp[i][j] = dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1] + table[i-1][j-1]
     }
 }
 
+var result = [Int]()
 for _ in 0..<M {
-    let xy = readLine()!.split(separator: " ").map { Int($0)! }
-    let x1 = xy[0]
-    let y1 = xy[1]
-    let x2 = xy[2]
-    let y2 = xy[3]
+    let testCase = readLine()!.split(separator: " ").map { Int($0)! }
+    let (x1, y1, x2, y2) = (testCase[0], testCase[1], testCase[2], testCase[3])
     
-    print(table[x2][y2] - table[x1-1][y2] - table[x2][y1-1] + table[x1-1][y1-1])
+    let sum = dp[x2][y2] - dp[x1-1][y2] - dp[x2][y1-1] + dp[x1-1][y1-1]
+    result.append(sum)
 }
+result.forEach { print($0) }
