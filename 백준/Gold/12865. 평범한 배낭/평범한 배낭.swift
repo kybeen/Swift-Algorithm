edadb7c032927768467301d17778b909cba5134b
김영빈 (Rei) <89764127+kybeen@users.noreply.github.com>
2025-02-07 18:36:10 +0900

    [Gold V] Title: 평범한 배낭, Time: 296 ms, Memory: 147508 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.swift
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.swift
@@ -4,22 +4,21 @@ var weight = [Int]()
 var value = [Int]()
 for _ in 0..<N {
     let WV = readLine()!.split(separator: " ").map { Int($0)! }
-    let (W, V) = (WV[0], WV[1])
-    weight.append(W)
-    value.append(V)
+    weight.append(WV[0])
+    value.append(WV[1])
 }
 
+// dp[i][j]: 0~i번 물건까지 고려했을 때 j무게까지 담을 수 있는 최대 가치
 var dp = Array(repeating: Array(repeating: 0, count: K+1), count: N)
-for j in 0...K {
-    if weight[0] <= j {
+for j in 1...K {
+    if j >= weight[0] {
         dp[0][j] = value[0]
     }
 }
-
 if N > 1 {
     for i in 1..<N {
         for j in 1...K {
-            if j - weight[i] >= 0 {
+            if j >= weight[i] {
                 dp[i][j] = max(dp[i-1][j], dp[i-1][j-weight[i]] + value[i])
             } else {
                 dp[i][j] = dp[i-1][j]
