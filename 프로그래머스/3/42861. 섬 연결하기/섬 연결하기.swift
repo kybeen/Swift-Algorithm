4df050de2e76ac4940aabc071802c54aa982aaed
김영빈 (Rei) <89764127+kybeen@users.noreply.github.com>
2025-02-12 15:51:40 +0900

    [level 3] Title: 섬 연결하기, Time: 0.27 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.swift
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.swift
@@ -1,17 +1,18 @@
 import Foundation
 
 func solution(_ n:Int, _ costs:[[Int]]) -> Int {
-    var roots = Array(0..<n)
+    var sortedEdges = costs.sorted { $0[2] < $1[2] }
+    var root = Array(0..<n)
     var result = 0
     
-    let sortedEdges = costs.sorted { $0[2] < $1[2] }
-    
-    func updateRoot(from: Int, to: Int) {
-        let target = roots[from]
-        let updatedRoot = roots[to]
+    // of 그룹의 루트를 모두 to 그룹의 루트로 업데이트
+    func updateRoot(of: Int, to: Int) {
+        let rootToUpdate = root[of]
+        let target = root[to]
+        
         for i in 0..<n {
-            if roots[i] == target {
-                roots[i] = updatedRoot
+            if root[i] == rootToUpdate {
+                root[i] = target
             }
         }
     }
@@ -21,10 +22,10 @@ func solution(_ n:Int, _ costs:[[Int]]) -> Int {
         let b = edge[1]
         let cost = edge[2]
         
-        if roots[a] != roots[b] {
+        // 사이클이 아니라면 다리를 건설 + 두 노드의 루트를 하나로 통일
+        if root[a] != root[b] {
+            updateRoot(of: a, to: b)
             result += cost
-            // roots[b] = roots[a]
-            updateRoot(from: a, to: b)
         }
     }
     
