09b3b6fe7d752c34bb42442427c42a6ebf9dfdd2
김영빈 (Rei) <89764127+kybeen@users.noreply.github.com>
2025-02-12 17:11:39 +0900

    [level 3] Title: 섬 연결하기, Time: 0.30 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.swift
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.swift
@@ -2,29 +2,38 @@ import Foundation
 
 func solution(_ n:Int, _ costs:[[Int]]) -> Int {
     var sortedEdges = costs.sorted { $0[2] < $1[2] }
-    var root = Array(0..<n)
+    var parent = Array(0..<n)
+    var rank = Array(repeating: 0, count: n)
     var result = 0
     
-    // of 그룹의 루트를 모두 to 그룹의 루트로 업데이트
-    func updateRoot(of: Int, to: Int) {
-        let rootToUpdate = root[of]
-        let target = root[to]
-        
-        for i in 0..<n {
-            if root[i] == rootToUpdate {
-                root[i] = target
-            }
+    func union(_ rootA: Int, _ rootB: Int) {
+        if rank[rootA] < rank[rootB] {
+            parent[rootA] = rootB
+        } else if rank[rootB] < rank[rootA] {
+            parent[rootB] = rootA
+        } else {
+            parent[rootB] = rootA
+            rank[rootA] += 1
+        }
+    }
+    
+    func find(_ node: Int) -> Int {
+        if parent[node] == node {
+            return node
+        } else {
+            parent[node] = find(parent[node])
+            return parent[node]
         }
     }
     
     for edge in sortedEdges {
-        let a = edge[0]
-        let b = edge[1]
+        let rootA = find(edge[0])
+        let rootB = find(edge[1])
         let cost = edge[2]
         
         // 사이클이 아니라면 다리를 건설 + 두 노드의 루트를 하나로 통일
-        if root[a] != root[b] {
-            updateRoot(of: a, to: b)
+        if rootA != rootB {
+            union(rootA, rootB)
             result += cost
         }
     }
