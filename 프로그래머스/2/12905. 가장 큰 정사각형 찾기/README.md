aa503e4577cf895f9c5d9e0ac7b57d5ab3e33b9a
김영빈 (Rei) <89764127+kybeen@users.noreply.github.com>
2025-02-18 16:40:22 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 150.75 ms, Memory: 63.1 MB -BaekjoonHub

--- 프로그래머스/2/12905. 가장 큰 정사각형 찾기/README.md
+++ 프로그래머스/2/12905. 가장 큰 정사각형 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 가장 큰 정사각형 찾기 - 12905 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12905?language=swift) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12905) 
 
 ### 성능 요약
 
-메모리: 62.7 MB, 시간: 136.36 ms
+메모리: 63.1 MB, 시간: 150.75 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 16일 11:11:21
+2025년 02월 18일 16:40:20
 
 ### 문제 설명
 
--- 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.swift
+++ 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.swift
@@ -1,20 +1,22 @@
 import Foundation
 
 func solution(_ board:[[Int]]) -> Int {
-    let rowCnt = board.count
-    let colCnt = board[0].count
+    let rowCount = board.count
+    let colCount = board[0].count
     var dp = board
-    var maxSquareLength = 0
     
-    // 입력이 1개 행밖에 없는 경우 예외처리
-    if rowCnt == 1 {
+    if rowCount == 1 {
         return dp[0].max()!
     }
     
-    for i in 1..<rowCnt {
-        for j in 1..<colCnt {
+    var maxSquareLength = 0
+    for i in 1..<rowCount {
+        for j in 1..<colCount {
+            let left = dp[i][j-1]
+            let up = dp[i-1][j]
+            let leftUp = dp[i-1][j-1]
             if board[i][j] == 1 {
-                dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + 1
+                dp[i][j] = min(left, up, leftUp) + 1
                 maxSquareLength = max(maxSquareLength, dp[i][j])
             }
         }
