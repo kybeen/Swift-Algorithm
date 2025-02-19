1aa4cd542069846ed1d098bdd6b91dba49e13d5c
김영빈 (Rei) <89764127+kybeen@users.noreply.github.com>
2025-02-19 14:26:30 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 368.41 ms, Memory: 16.9 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.swift
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.swift
@@ -1,12 +1,6 @@
 import Foundation
 
-/// 코너 여부를 확인하기 위한 좌표 방향
-enum Direction {
-    case vertical
-    case horizontal
-}
-
-struct Point: Hashable {
+struct Point: Equatable {
     let i: Int
     let j: Int
     
@@ -14,76 +8,58 @@ struct Point: Hashable {
         self.i = i
         self.j = j
     }
+    
+    func getDirection(to: Point) -> Direction {
+        return (self.i == to.i) && (self.j != to.j) ? .horizontal : .vertical
+    }
 }
 
-struct QueueItem: Hashable {
-    let point: Point // 현재 좌표
-    let dir: Direction? // 현재 좌표로 진행해온 방향
-    let sum: Int // 현재까지 건설 비용의 합
-    
-    init(
-        _ point: Point,
-        _ dir: Direction?,
-        _ sum: Int
-    ) {
-        self.point = point
-        self.dir = dir
-        self.sum = sum
-    }
-}
-    
-/// 현재 진행 방향과 코너 여부를 계산하여 반환
-func checkDirectionAndCorner(_ now: Point, _ next: Point, prevDir: Direction?) -> (Direction, Bool) {
-    let nextDir: Direction = (now.i == next.i) ? .vertical : .horizontal
-    if let prevDir = prevDir {
-        return (nextDir, nextDir != prevDir)
-    } else {
-        // (0,0)에서 시작하는 상황에서는 prevDir가 없음
-        return (nextDir, false)
-    }
+enum Direction {
+    case vertical
+    case horizontal
 }
 
 func solution(_ board:[[Int]]) -> Int {
-    let N = board.count
-    var cost = Array(repeating: Array(repeating: [Int.max, Int.max], count: N), count: N)
-    var idx = 0
-    var queue = [QueueItem]()
-    
-    let startPoint = Point(0, 0)
-    let startItem = QueueItem(startPoint, nil, 0)
-    cost[0][0][0] = 0
-    cost[0][0][1] = 0
-    queue.append(startItem)
-    
+    let length = board.count
     let di = [-1, 1, 0, 0]
     let dj = [0, 0, -1, 1]
+    var cost = Array(repeating: Array(repeating: [Int.max, Int.max], count: length), count: length)
     
-    while idx < queue.count {
-        let now = queue[idx]
+    func dfs(_ now: Point, _ nowDirection: Direction, _ nowCost: Int) {
+        if now == Point(length-1, length-1) {
+            return
+        }
         
         for k in 0..<4 {
-            let nextI = now.point.i + di[k]
-            let nextJ = now.point.j + dj[k]
+            let next = Point(now.i+di[k], now.j+dj[k])
+            guard next.i >= 0 && next.j >= 0 && next.i < length && next.j < length else { continue }
             
-            guard nextI >= 0 && nextJ >= 0 && nextI < N && nextJ < N else { continue }
-            let nextPoint = Point(nextI, nextJ)
-            if board[nextI][nextJ] == 0 {
-                let (nextDir, isCorner) = checkDirectionAndCorner(now.point,
-                                                                  nextPoint,
-                                                                  prevDir: now.dir)
-                let additionalCost = 100 + (isCorner ? 500 : 0)
-                let nextSum = now.sum + additionalCost
-                let dirIdx = (nextDir == .horizontal) ? 0 : 1
-                if nextSum <= cost[nextI][nextJ][dirIdx] {
-                    cost[nextI][nextJ][dirIdx] = nextSum
-                    let nextQueueItem = QueueItem(nextPoint, nextDir, nextSum)
-                    queue.append(nextQueueItem)
+            if board[next.i][next.j] == 0 {
+                let nextDirection = now.getDirection(to: next)
+                let nextCost = nowCost + (nowDirection == nextDirection ? 100 : 600)
+                let dirIdx = (nextDirection == .vertical) ? 0 : 1
+                
+                if nextCost < cost[next.i][next.j][dirIdx] {
+                    cost[next.i][next.j][dirIdx] = nextCost
+                    dfs(next, nextDirection, nextCost)
                 }
             }
         }
-        
-        idx += 1
     }
     
-    return min(cost[N-1][N-1][0], cost[N-1][N-1][1])
+    cost[0][0][0] = 0
+    cost[0][0][1] = 0
+    
+    // 오른쪽으로 시작
+    if board[0][1] == 0 {
+        cost[0][1][0] = 100
+        dfs(Point(0,1), .horizontal, 100)
+    }
+    // 아래로 시작
+    if board[1][0] == 0 {
+        cost[1][0][1] = 100
+        dfs(Point(1,0), .vertical, 100)
+    }
+    
+    return min(cost[length-1][length-1][0], cost[length-1][length-1][1])
 }
