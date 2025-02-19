3fd7c9b5b5f7bc89031050d244a83b4cfef7c01c
김영빈 (Rei) <89764127+kybeen@users.noreply.github.com>
2025-02-19 12:46:35 +0900

    [level 2] Title: 피로도, Time: 29.94 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.swift
+++ 프로그래머스/2/87946. 피로도/피로도.swift
@@ -1,72 +1,27 @@
-// import Foundation
-
-// func solution(_ k:Int, _ dungeons:[[Int]]) -> Int {
-//     let n = dungeons.count
-//     var visited = [Bool](repeating: false, count: n)
-//     var result = -1
-    
-//     // rest: 잔여 피로도, depth: 탐험한 던전 수
-//     func dfs(_ rest: Int, _ depth: Int) {
-//         // 최대 탐험 던전 수 갱신
-//         result = max(result, depth)
-        
-//         for i in 0..<n {
-//             // 방문하지 않았고 잔여 피로도가 최소 필요 피로도 이상이면 던전 탐험
-//             if !visited[i] && dungeons[i][0] <= rest {
-//                 visited[i] = true
-//                 dfs(rest - dungeons[i][1], depth+1)
-//                 visited[i] = false
-//             }
-//         }
-//     }
-    
-//     dfs(k, 0)
-    
-//     return result
-// }
-
 import Foundation
 
 func solution(_ k:Int, _ dungeons:[[Int]]) -> Int {
-    var visited: [Bool] = Array(repeating: false, count: dungeons.count)
-    var result: Int = 0
+    let dungeonCount = dungeons.count
+    var visited = Array(repeating: false, count: dungeonCount)
+    var result = 0
     
-    func dfs(_ depth: Int, _ fatigue: Int) {
+    func dfs(_ energy: Int, _ depth: Int) {
         result = max(result, depth)
+        guard depth < dungeonCount else { return }
         
-        for i in 0..<dungeons.count {
-            let dungeon = dungeons[i]
-            if !visited[i] && (fatigue >= dungeon[0]) {
-                visited[i] = true
-                dfs(depth+1, fatigue-dungeon[1])
-                visited[i] = false
+        for next in 0..<dungeonCount {
+            let nextNeed = dungeons[next][0]
+            let nextUse = dungeons[next][1]
+            
+            if !visited[next] && energy >= nextNeed {
+                visited[next] = true
+                dfs(energy-nextUse, depth+1)
+                visited[next] = false
             }
         }
     }
     
-    dfs(0, k)
+    dfs(k, 0)
     
     return result
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
