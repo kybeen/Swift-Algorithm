7e65cce1955cda51e4d84b646c22d7fba40cb7d8
김영빈 (Rei) <89764127+kybeen@users.noreply.github.com>
2025-02-20 15:51:54 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.01 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.swift
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.swift
@@ -1,18 +1,17 @@
 import Foundation
 
 func solution(_ n:Int) -> Int {
+    var answer = 1
     var n = n
-    var result = 0
     
-    while n != 0 {
+    while n > 1 {
         if n % 2 == 0 {
             n /= 2
         } else {
             n -= 1
-            n /= 2
-            result += 1
+            answer += 1
         }
     }
-    
-    return result
+
+    return answer
 }
