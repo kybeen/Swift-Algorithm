e213690907338411ae6b13bb2110165fcffe2197
김영빈 (Rei) <89764127+kybeen@users.noreply.github.com>
2025-02-21 14:30:25 +0900

    [level 2] Title: 가장 큰 수, Time: 836.77 ms, Memory: 22.2 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.swift
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.swift
@@ -1,9 +1,9 @@
 import Foundation
 
 func solution(_ numbers:[Int]) -> String {
-    let sortedNumbers = numbers.sorted {
-        return Int("\($0)\($1)")! > Int("\($1)\($0)")!
+    var sortedNumbers = numbers.sorted { "\($0)\($1)" >= "\($1)\($0)" }
+    while let first = sortedNumbers.first, first == 0, sortedNumbers.count > 1 {
+        _ = sortedNumbers.removeFirst()
     }
-    let result = sortedNumbers.map({ String($0) }).joined(separator: "")
-    return sortedNumbers[0] == 0 ? "0" : result
+    return sortedNumbers.map { String($0) }.joined()
 }
