dbd1f9edf0e44b8aa3acaaa5c0e94d432837458f
김영빈 (Rei) <89764127+kybeen@users.noreply.github.com>
2025-02-24 11:07:46 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.swift
+++ 프로그래머스/2/42842. 카펫/카펫.swift
@@ -1,20 +1,17 @@
 import Foundation
 
 func solution(_ brown:Int, _ yellow:Int) -> [Int] {
-    let area = brown + yellow // 가로x세로 == brown+yellow == 넓이
-    let maxHeight = Int(sqrt(Double(area)))
-    var result = [0, 0]
+    var answer = [Int]()
     
-    // 세로는 항상 가로 이하이기 때문에 넓이의 제곱근까지만 확인
-    for height in 3...maxHeight {
-        let yellowHeight = height - 2
-        let yellowWidth = yellow / (yellowHeight)
-        let yellowCount = yellowHeight * yellowWidth
-        let brownCount = (yellowHeight*2) + (yellowWidth*2) + 4
-        if yellowCount == yellow && brownCount == brown {
-            result = [area / height, height]
+    for yellowHeight in 1...Int(sqrt(Double(yellow))) {
+        if yellow % yellowHeight == 0 {
+            let yellowWidth = yellow / yellowHeight
+            
+            if (yellowHeight*2) + (yellowWidth*2) + 4 == brown {
+                answer = [yellowWidth+2, yellowHeight+2]
+                break
+            }
         }
     }
-    
-    return result
+    return answer
 }
