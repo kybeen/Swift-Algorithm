a05e7cbff30933406edcc2a728d19032459e1b14
김영빈 (Rei) <89764127+kybeen@users.noreply.github.com>
2025-03-20 18:25:40 +0900

    [Gold V] Title: 빗물, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.swift
+++ 백준/Gold/14719. 빗물/빗물.swift
@@ -1,39 +1,37 @@
 let HW = readLine()!.split(separator: " ").map({ Int($0)! })
 let (H, W) = (HW[0], HW[1])
-let blocks = readLine()!.split(separator: " ").map({ Int($0)! })
+var blocks = readLine()!.split(separator: " ").map({ Int($0)! })
 
-// i번째 칸의 왼쪽/오른쪽에서 가장 높은 블록
-var leftHighest = Array<Int>(repeating: 0, count: blocks.count)
-var rightHighest = Array<Int>(repeating: 0, count: blocks.count)
+var leftMaxHeight = Array(repeating: 0, count: W)
+var rightMaxHeight = Array(repeating: 0, count: W)
 
-var answer = 0
-
-if W > 1 {
-    var right = 1
-    var lHighest = blocks[0]
-    while right < blocks.count {
-        leftHighest[right] = lHighest
-        if blocks[right] > lHighest {
-            lHighest = blocks[right]
-        }
-        right += 1
+// 각 위치를 기준으로 가장 높은 왼쪽 블록 높이 구하기
+var left = 0
+var right = 0
+while right < W {
+    if blocks[left] > blocks[right] {
+        leftMaxHeight[right] = blocks[left]
+    } else {
+        left = right
     }
-    
-    var left = blocks.count - 2
-    var rHighest = blocks[blocks.count-1]
-    while left >= 0 {
-        rightHighest[left] = rHighest
-        if blocks[left] > rHighest {
-            rHighest = blocks[left]
-        }
-        left -= 1
-    }
-    
-    for i in 0..<blocks.count {
-        let upperBound = min(leftHighest[i], rightHighest[i])
-        let pond = max(0, upperBound - blocks[i])
-        answer += pond
-    }
+    right += 1
 }
 
-print(answer)
+// 각 위치를 기준으로 가장 높은 오른쪽 블록 높이 구하기
+left = W - 1
+right = W - 1
+while left >= 0 {
+    if blocks[right] > blocks[left] {
+        rightMaxHeight[left] = blocks[right]
+    } else {
+        right = left
+    }
+    left -= 1
+}
+
+var result = 0
+for i in 0..<W {
+    let upperBound = min(leftMaxHeight[i], rightMaxHeight[i])
+    result += max(0, upperBound - blocks[i])
+}
+print(result)
