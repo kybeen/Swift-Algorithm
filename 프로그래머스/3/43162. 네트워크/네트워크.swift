26f2c1b9894b9b8d58e51c8fc1976ca8072b5d50
김영빈 (Rei) <89764127+kybeen@users.noreply.github.com>
2025-04-11 15:27:06 +0900

    [level 3] Title: 네트워크, Time: 2.37 ms, Memory: 16.9 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.swift
+++ 프로그래머스/3/43162. 네트워크/네트워크.swift
@@ -1,81 +1,30 @@
 import Foundation
 
 func solution(_ n:Int, _ computers:[[Int]]) -> Int {
-    var nodes = [[Int]](repeating: [Int](), count: n)
-    var visited = [Bool](repeating: false, count: n)
-    var result = 0
+    var visited = Array(repeating: false, count: n)
+    var answer = 0
     
-    // 노드 연결 관계 만들기
-    for i in 0..<n {
-        for j in 0..<n {
-            if i != j && computers[i][j] == 1 {
-                nodes[i].append(j)
-            }
-        }
-    }
-    
-    // DFS 수행하며 방문처리
-    func dfs(_ now: Int) {
-        for node in nodes[now] {
-            if !visited[node] {
-                visited[node] = true
-                dfs(node)
-            }
-        }
-    }
-    
-    // 방문하지 않은 노드를 발견하면 네트워크 개수 1 카운트하고, DFS 시작
     for i in 0..<n {
         if !visited[i] {
-            result += 1
-            dfs(i)
+            answer += 1
+            var idx = 0
+            var queue = [Int]()
+            visited[i] = true
+            queue.append(i)
+            
+            while idx < queue.count {
+                let now = queue[idx]
+                for (next, isConnected) in computers[now].enumerated() {
+                    if isConnected == 1 && !visited[next] {
+                        visited[next] = true
+                        queue.append(next)
+                    }
+                }
+                
+                idx += 1
+            }
         }
     }
     
-    return result
+    return answer
 }
-
-
-
-// import Foundation
-
-// func solution(_ n:Int, _ computers:[[Int]]) -> Int {
-//     var visited: [Bool] = Array(repeating: false, count: n)
-//     var result = 0
-    
-//     func dfs(_ now: Int) {
-//         visited[now] = true
-        
-//         for i in 0..<n {
-//             if (computers[now][i] == 1) && !visited[i] {
-//                 dfs(i)
-//             }
-//         }
-//     }
-    
-//     for i in 0..<n {
-//         if !visited[i] {
-//             result += 1
-//             dfs(i)
-//         }
-//     }
-    
-//     return result
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
