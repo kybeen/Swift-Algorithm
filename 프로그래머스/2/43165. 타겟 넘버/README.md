44a7873f16a59abcb02c50371311c1e088f992b6
김영빈 (Rei) <89764127+kybeen@users.noreply.github.com>
2025-04-11 15:21:19 +0900

    [level 2] Title: 타겟 넘버, Time: 42.78 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=swift) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
 
 ### 성능 요약
 
-메모리: 16.4 MB, 시간: 0.86 ms
+메모리: 16.4 MB, 시간: 42.78 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 4일 3:45:10
+2025년 04월 11일 15:21:16
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.swift
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.swift
@@ -1,32 +1,21 @@
 import Foundation
 
 func solution(_ numbers:[Int], _ target:Int) -> Int {
-    let n = numbers.count
-    var result = 0
+    var answer = 0
     
-    var queue = [(Int, Int)]() // (현재인덱스, 계산 결과)
-    var idx = 0
-    queue.append((0, numbers[0]))
-    queue.append((0, -numbers[0]))
-    
-    while idx < queue.count {
-        let now = queue[idx]
-        
-        // 마지막 수까지 계산했을 때
-        if now.0 == n-1 {
-            // target과 계산 결과가 일치하면 result 1 증가
-            if now.1 == target {
-                result += 1
+    func dfs(_ now: Int, _ sum: Int) {
+        if now == numbers.count {
+            if sum == target {
+                answer += 1
             }
+            return
         }
         
-        if now.0 < n-1 {
-            queue.append((now.0+1, now.1+numbers[now.0+1]))
-            queue.append((now.0+1, now.1-numbers[now.0+1]))
-        }
-        
-        idx += 1
+        dfs(now+1, sum+numbers[now])
+        dfs(now+1, sum-numbers[now])
     }
     
-    return result
+    dfs(0, 0)
+    
+    return answer
 }
