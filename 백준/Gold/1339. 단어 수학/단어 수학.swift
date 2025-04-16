9a3efa7e249f064cd4668ba39cf6b3cacf3a22be
김영빈 (Rei) <89764127+kybeen@users.noreply.github.com>
2025-04-16 11:11:46 +0900

    [Gold IV] Title: 단어 수학, Time: 8 ms, Memory: 79516 KB -BaekjoonHub

--- 백준/Gold/1339. 단어 수학/단어 수학.swift
+++ 백준/Gold/1339. 단어 수학/단어 수학.swift
@@ -1,29 +1,19 @@
-/*
- 자릿수가 높을수록 큰 숫자가 들어가야 함
- - ABC + BDE = 110B + 100A + 10D + C + E
- => B:9, A:8, D:7, ... 이런 식으로
- */
 import Foundation
 
 let N = Int(readLine()!)!
-var dict = [Character: Int]() // [문자: 자릿수값]
+var priority = [Character: Int]() // 각 알파벳의 자릿수 기반 가중치
 for _ in 0..<N {
-    let word = Array(readLine()!)
-    for i in 0..<word.count {
-        if dict[word[i]] == nil {
-            dict[word[i]] = Int(pow(10, Double(word.count-i-1)))
-        } else {
-            dict[word[i]]! += Int(pow(10, Double(word.count-i-1)))
-        }
+    let word = readLine()!
+    for (idx, chr) in word.reversed().enumerated() {
+        priority[chr, default: 0] += (Int(pow(Double(10), Double(idx))))
     }
 }
+//print(priority)
 
-var result = 0
-var num = 9
-
-// value가 큰 순서대로 9,8,7... 순서대로 큰 값부터 곱해서 더한다.
-for (_, value) in dict.sorted(by: { $0.value > $1.value }) {
-    result += value * num
-    num -= 1
+var answer = 0
+var bestNumber = 9
+for (_, priority) in priority.sorted(by: { $0.value > $1.value }) {
+    answer += (bestNumber * priority)
+    bestNumber -= 1
 }
-print(result)
+print(answer)
