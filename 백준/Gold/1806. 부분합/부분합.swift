598bfcd6d0f64b4727564c16a54f0aa54f844041
김영빈 (Rei) <89764127+kybeen@users.noreply.github.com>
2025-04-24 17:57:07 +0900

    [Gold IV] Title: 부분합, Time: 44 ms, Memory: 86700 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.swift
+++ 백준/Gold/1806. 부분합/부분합.swift
@@ -4,26 +4,18 @@ let NS = readLine()!.split(separator: " ").map({ Int($0)! })
 let (N, S) = (NS[0], NS[1])
 let arr = readLine()!.split(separator: " ").map({ Int($0)! })
 
-var nowSum = 0
-for num in arr {
-    nowSum += num
-}
-
 var answer = Int.max
-var left = 0
+var sum = 0
 var right = 0
-nowSum = 0
-
-while true {
-    if nowSum >= S {
-        answer = min(answer, right - left)
-        nowSum -= arr[left]
-        left += 1
-    } else {
-        if right == N { break }
-        nowSum += arr[right]
+for left in 0..<N {
+    while sum < S && right < N {
+        sum += arr[right]
         right += 1
     }
+    if sum >= S {
+        let length = right - left
+        answer = min(answer, length)
+    }
+    sum -= arr[left]
 }
-
 print(answer == Int.max ? 0 : answer)
