0c51f9dcdb2110ef26ffebe93286c3f370b925d3
김영빈 (Rei) <89764127+kybeen@users.noreply.github.com>
2025-04-24 17:49:05 +0900

    [Silver IV] Title: 수들의 합 2, Time: 12 ms, Memory: 80420 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.swift
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.swift
@@ -3,23 +3,18 @@ import Foundation
 let NM = readLine()!.split(separator: " ").map({ Int($0)! })
 let (N, M) = (NM[0], NM[1])
 let A = readLine()!.split(separator: " ").map({ Int($0)! })
-var prefixSum = Array(repeating: 0, count: N+1)
-var sum = 0
-for i in 0..<N {
-    sum += A[i]
-    prefixSum[i+1] = sum
-}
-//print("prefixSum \(prefixSum)")
 
 var answer = 0
-for i in 1...N {
-    for j in i...N {
-        if prefixSum[j] - prefixSum[i-1] == M {
-            answer += 1
-        } else if prefixSum[j] - prefixSum[i-1] > M {
-            // 부분합이 M보다 커지는 구간부터는 j를 더 확인할 필요 x
-            break
-        }
+var sum = 0
+var right = 0
+for left in 0..<N {
+    while sum < M && right < N {
+        sum += A[right]
+        right += 1
     }
+    if sum == M {
+        answer += 1
+    }
+    sum -= A[left]
 }
 print(answer)
